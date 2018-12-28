2398d8dc6a4306a871d812ac280a237b25b4df73
201809_JMK <201809_JMK@d808369f-3623-194b-b0f8-88541be8a525>
2018-12-28 07:11:38 +0000

    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51407 d808369f-3623-194b-b0f8-88541be8a525

---
+++ Clap_Client/src/com/clap/vo/myalbum/MyAlbumVO.java
@@ -0,0 +1,35 @@
+package com.clap.vo.myalbum;
+
+public class MyAlbumVO {
+	
+	private String myalb_no;
+	private String myalb_name;
+	private String myalb_indate;
+	private String mem_id;
+	
+	public String getMyalb_no() {
+		return myalb_no;
+	}
+	public void setMyalb_no(String myalb_no) {
+		this.myalb_no = myalb_no;
+	}
+	public String getMyalb_name() {
+		return myalb_name;
+	}
+	public void setMyalb_name(String myalb_name) {
+		this.myalb_name = myalb_name;
+	}
+	public String getMyalb_indate() {
+		return myalb_indate;
+	}
+	public void setMyalb_indate(String myalb_indate) {
+		this.myalb_indate = myalb_indate;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+
+}
