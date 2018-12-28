637502b055ca2427926e335e4a51a7d53fcd0aa2
201809_JMK <201809_JMK@d808369f-3623-194b-b0f8-88541be8a525>
2018-12-28 08:07:49 +0000

    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51425 d808369f-3623-194b-b0f8-88541be8a525

---
+++ Clap_Client/src/com/clap/vo/myalbum/MyAlbumListVO.java
@@ -0,0 +1,30 @@
+package com.clap.vo.myalbum;
+
+import java.io.Serializable;
+
+public class MyAlbumListVO implements Serializable{
+	
+	private String myalb_no;
+	private String mus_no;
+	private String myalb_list_indate;
+	
+	public String getMyalb_no() {
+		return myalb_no;
+	}
+	public void setMyalb_no(String myalb_no) {
+		this.myalb_no = myalb_no;
+	}
+	public String getMus_no() {
+		return mus_no;
+	}
+	public void setMus_no(String mus_no) {
+		this.mus_no = mus_no;
+	}
+	public String getMyalb_list_indate() {
+		return myalb_list_indate;
+	}
+	public void setMyalb_list_indate(String myalb_list_indate) {
+		this.myalb_list_indate = myalb_list_indate;
+	}
+
+}
