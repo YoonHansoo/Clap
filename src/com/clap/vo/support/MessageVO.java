b1f2da8370e795354c08af1ea24cfd69f3c09635
201809_JMK <201809_JMK@d808369f-3623-194b-b0f8-88541be8a525>
2018-12-28 07:08:35 +0000

    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51405 d808369f-3623-194b-b0f8-88541be8a525

---
+++ src/com/clap/vo/support/MessageVO.java
@@ -0,0 +1,56 @@
+package com.clap.vo.support;
+
+public class MessageVO {
+	
+	private String msg_no;
+	private String msg_send_date;
+	private String msg_title;
+	private String msg_read_tf;
+	private String msg_content;
+	private String mem_send_id;
+	private String mem_get_id;
+	
+	public String getMsg_no() {
+		return msg_no;
+	}
+	public void setMsg_no(String msg_no) {
+		this.msg_no = msg_no;
+	}
+	public String getMsg_send_date() {
+		return msg_send_date;
+	}
+	public void setMsg_send_date(String msg_send_date) {
+		this.msg_send_date = msg_send_date;
+	}
+	public String getMsg_title() {
+		return msg_title;
+	}
+	public void setMsg_title(String msg_title) {
+		this.msg_title = msg_title;
+	}
+	public String getMsg_read_tf() {
+		return msg_read_tf;
+	}
+	public void setMsg_read_tf(String msg_read_tf) {
+		this.msg_read_tf = msg_read_tf;
+	}
+	public String getMsg_content() {
+		return msg_content;
+	}
+	public void setMsg_content(String msg_content) {
+		this.msg_content = msg_content;
+	}
+	public String getMem_send_id() {
+		return mem_send_id;
+	}
+	public void setMem_send_id(String mem_send_id) {
+		this.mem_send_id = mem_send_id;
+	}
+	public String getMem_get_id() {
+		return mem_get_id;
+	}
+	public void setMem_get_id(String mem_get_id) {
+		this.mem_get_id = mem_get_id;
+	}
+	
+}
