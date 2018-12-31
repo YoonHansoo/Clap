7494bdddc18ab48c1a9ce8de579f21072d86bf6e
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2018-12-31 09:53:22 +0000

    MusicMainController 추가
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51534 d808369f-3623-194b-b0f8-88541be8a525

---
+++ Clap_Client/src/kr/or/ddit/clap/main/MusicMainController.java
@@ -0,0 +1,24 @@
+package kr.or.ddit.clap.main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+/**
+ * 메인화면의 fxml 컨트롤러.
+ * @author Kyunghun
+ *
+ */
+public class MusicMainController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+	
+	public void login() {
+		System.out.println(111);
+	}
+
+}
