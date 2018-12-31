a969fb73e1ca8b8fd366ef8035c3575d08e27c82
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2018-12-31 09:59:13 +0000

    login창 생성
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51536 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/main/MusicMainController.java
+++ Clap_Client/src/kr/or/ddit/clap/main/MusicMainController.java
@@ -1,9 +1,16 @@
 package kr.or.ddit.clap.main;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * 메인화면의 fxml 컨트롤러.
@@ -11,14 +18,25 @@ import javafx.fxml.Initializable;
  *
  */
 public class MusicMainController implements Initializable{
+	
+	static Stage loginDialog = new Stage(StageStyle.DECORATED);
+//	static Stage joinDialog = new Stage(StageStyle.DECORATED);
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
 	}
 	
-	public void login() {
-		System.out.println(111);
+	public void login() throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+		Scene scene = new Scene(root);
+		loginDialog.setTitle("모여서 각잡고 코딩 - clap");
+		if(loginDialog.getModality() == null) {
+			loginDialog.initModality(Modality.APPLICATION_MODAL);			
+		}
+		
+		loginDialog.setScene(scene);
+		loginDialog.show();
 	}
 
 }
