819c8ba0a12cbedf761367bb5af2f2528dda7685
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2018-12-31 10:30:08 +0000

    로그인 버튼 수정
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51552 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/main/LoginController.java
+++ Clap_Client/src/kr/or/ddit/clap/main/LoginController.java
@@ -3,13 +3,25 @@ package kr.or.ddit.clap.main;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class LoginController implements Initializable{
+	
+	@FXML JFXTextField txt_id;
+	@FXML JFXPasswordField txt_pw;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
+	}
+	
+	public void login() {
+		System.out.println(txt_id.getText());
+		System.out.println(txt_pw.getText());
 	}
 
 }
