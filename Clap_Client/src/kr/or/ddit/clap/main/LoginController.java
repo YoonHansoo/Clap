20a0776b3dbc303b21978700a3c9ccb6c6c2f24b
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2018-12-31 10:07:19 +0000

    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51538 d808369f-3623-194b-b0f8-88541be8a525

---
+++ Clap_Client/src/kr/or/ddit/clap/main/LoginController.java
@@ -0,0 +1,15 @@
+package kr.or.ddit.clap.main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class LoginController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+
+}
