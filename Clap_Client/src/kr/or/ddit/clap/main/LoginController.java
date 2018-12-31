58fb8663481c2e5f0108e3f95e45a7faeb5d570f
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2018-12-31 11:27:05 +0000

    pw 암/복호화 기능 추가
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51566 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/main/LoginController.java
+++ Clap_Client/src/kr/or/ddit/clap/main/LoginController.java
@@ -1,6 +1,9 @@
 package kr.or.ddit.clap.main;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXPasswordField;
@@ -9,19 +12,35 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+/**
+ * 로그인창 컨트롤러.
+ * @author Kyunghun
+ *
+ */
 public class LoginController implements Initializable{
 	
 	@FXML JFXTextField txt_id;
 	@FXML JFXPasswordField txt_pw;
+	
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
 	}
 	
-	public void login() {
+	public void login() throws UnsupportedEncodingException, NoSuchAlgorithmException, GeneralSecurityException {
+		AES256Util aes = new AES256Util();
+		
+		String encryptedPw = ""; // 암호화된 pw
+		encryptedPw = aes.encrypt(txt_pw.getText());
+		
+		String decryptedPw = ""; // 복호화시킨 pw
+		decryptedPw = aes.decrypt(encryptedPw);
+				
 		System.out.println(txt_id.getText());
-		System.out.println(txt_pw.getText());
+		System.out.println(encryptedPw);
+		System.out.println(decryptedPw);
+		
 	}
 
 }
