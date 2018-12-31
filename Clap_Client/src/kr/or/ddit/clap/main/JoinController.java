205057a848e0ecb28f23b96bac0d63f1cd8bbf38
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2018-12-31 12:03:03 +0000

    회원가입 수정
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51577 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/main/JoinController.java
+++ Clap_Client/src/kr/or/ddit/clap/main/JoinController.java
@@ -1,8 +1,15 @@
 package kr.or.ddit.clap.main;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -11,10 +18,39 @@ import javafx.fxml.Initializable;
  *
  */
 public class JoinController implements Initializable{
+	
+	@FXML JFXTextField txt_id;
+	@FXML JFXTextField txt_pw;
+	@FXML JFXTextField txt_pwCheck;
+	@FXML JFXTextField txt_birth;
+	@FXML JFXTextField txt_tel;
+	@FXML JFXTextField txt_email;
+	
+	@FXML JFXButton btn_ok;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
 	}
 
+	public void join() throws UnsupportedEncodingException, NoSuchAlgorithmException, GeneralSecurityException {
+		AES256Util aes = new AES256Util();
+		
+		String encryptedPw = aes.encrypt(txt_pw.getText());
+		String decryptedPw = aes.decrypt(encryptedPw);
+		
+		String encryptedPwCheck = aes.encrypt(txt_pwCheck.getText());
+		String decryptedPwCheck = aes.decrypt(encryptedPwCheck);
+		
+		System.out.println("아이디:"+txt_id.getText());
+		System.out.println("비번:"+txt_pw.getText());
+		System.out.println("비번확인:"+txt_pwCheck.getText());
+		System.out.println("암호화 비번:"+encryptedPw);
+		System.out.println("암호화 비번확인:"+encryptedPwCheck);
+		
+		System.out.println("생일:"+txt_birth.getText());
+		System.out.println("번호:"+txt_tel.getText());
+		System.out.println("이메일:"+txt_email.getText());
+	}
+	
 }
