051b62cd313d017a431183f9c6a7288b8a595a76
201809_YHS <201809_YHS@d808369f-3623-194b-b0f8-88541be8a525>
2018-12-31 07:51:06 +0000

    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51498 d808369f-3623-194b-b0f8-88541be8a525

---
+++ Clap_Server/src/kr/or/ddit/clap/main/ServerMain.java
@@ -0,0 +1,28 @@
+package kr.or.ddit.clap.main;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class ServerMain {
+	
+	
+	
+	public static void main(String[] args) {
+		try {
+//			IBoardService bsi = new BoardServiceImpl();
+			
+			Registry reg = LocateRegistry.createRegistry(7777);
+			
+//			reg.rebind("board", bsi);
+			  System.out.println("ChatServerImpl is running...");
+			  
+		} catch (Exception e) {
+			System.out.println("마 에러다!");
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+
+}
---
+++ Clap_Server/src/kr/or/ddit/clap/vo/album/AlbumReviewVO.java
@@ -0,0 +1,44 @@
+package kr.or.ddit.clap.vo.album;
+
+import java.io.Serializable;
+
+public class AlbumReviewVO implements Serializable{
+	
+	private String alb_re_no;
+	private String alb_re_content;
+	private String alb_re_indate;
+	private String mem_id;
+	private String alb_no;
+	
+	public String getAlb_re_no() {
+		return alb_re_no;
+	}
+	public void setAlb_re_no(String alb_re_no) {
+		this.alb_re_no = alb_re_no;
+	}
+	public String getAlb_re_content() {
+		return alb_re_content;
+	}
+	public void setAlb_re_content(String alb_re_content) {
+		this.alb_re_content = alb_re_content;
+	}
+	public String getAlb_re_indate() {
+		return alb_re_indate;
+	}
+	public void setAlb_re_indate(String alb_re_indate) {
+		this.alb_re_indate = alb_re_indate;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getAlb_no() {
+		return alb_no;
+	}
+	public void setAlb_no(String alb_no) {
+		this.alb_no = alb_no;
+	}
+	
+}
