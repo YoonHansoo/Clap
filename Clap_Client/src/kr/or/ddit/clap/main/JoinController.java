dd3af83edfafca17dd82210ab93a4d12cdf06985
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2018-12-31 10:06:53 +0000

    회원가입 컨트롤러 생성
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51537 d808369f-3623-194b-b0f8-88541be8a525

---
+++ Clap_Client/src/kr/or/ddit/clap/main/JoinController.java
@@ -0,0 +1,20 @@
+package kr.or.ddit.clap.main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+/**
+ * 회원가입 창 컨트롤러.
+ * @author Kyunghun
+ *
+ */
+public class JoinController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+
+}
