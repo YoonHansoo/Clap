8eab3921f65f1ba43d6236faba404559bbadeff3
201809_JMK <201809_JMK@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-01 07:35:06 +0000

    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51652 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/view/chartmenu/main/ChartMenuController.java
+++ Clap_Client/src/kr/or/ddit/clap/view/chartmenu/main/ChartMenuController.java
@@ -1,14 +1,18 @@
 package kr.or.ddit.clap.view.chartmenu.main;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
 import com.jfoenix.controls.JFXTabPane;
 
 import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.AnchorPane;
 
 public class ChartMenuController implements Initializable{
 	
@@ -20,13 +24,33 @@ public class ChartMenuController implements Initializable{
 	@FXML Tab tab_period;
 	@FXML Tab tab_musicvideo;
 
+	@FXML AnchorPane anchorPane_top50;
+
 	
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
 		tabPane_main.getSelectionModel().select(menuCount);
-		
+		try {
+			switch(menuCount) {
+				case 0:
+					AnchorPane pane = FXMLLoader.load(getClass().getResource("../top50/Top50RealTime.fxml"));
+					anchorPane_top50.getChildren().removeAll();
+					anchorPane_top50.getChildren().setAll(pane);
+					break;
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 		
