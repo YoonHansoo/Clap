e5a02a558a936452d560a6bb9d4fd6b5d02562c8
201809_KHJ <201809_KHJ@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-01 14:47:36 +0000

    주석처리
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51679 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/view/member/mypage/MypageController.java
+++ Clap_Client/src/kr/or/ddit/clap/view/member/mypage/MypageController.java
@@ -25,13 +25,13 @@ public class MypageController implements Initializable {
 		
 		String user_id= LoginSession.session.getMem_id();
 		
-		userid.setText(user_id);
+		userid.setText(user_id);	//현재 로그인한 사용자 아이디 가져오기
 		
 	
 	}
 	
 	@FXML
-	public void btn_profch() throws IOException {
+	public void btn_profch() throws IOException {	//프로필 수정 클릭시
 		Parent root = FXMLLoader.load(getClass().getResource("propli.fxml"));
 		Scene scene = new Scene(root);
 		mypageDialog.setTitle("모여서 각잡고 코딩 - clap");
