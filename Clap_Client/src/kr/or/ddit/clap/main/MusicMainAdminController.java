411172b014457deaed9fa3ff7422c324553bc7cc
201809_JMK <201809_JMK@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-01 05:23:43 +0000

    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51625 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/main/MusicMainAdminController.java
+++ Clap_Client/src/kr/or/ddit/clap/main/MusicMainAdminController.java
@@ -93,4 +93,46 @@ public class MusicMainAdminController implements Initializable {
 		
 	}
 	
+	@FXML
+	public void genrePageChange(ActionEvent event) { //차트메뉴에서 Top50차트 클릭 했을때 페이지 전환 이벤트
+		try {
+			Parent top50Page = FXMLLoader.load(getClass().getResource("../view/chartmenu/main/ChartMenu.fxml")); //바뀔 화면을 가져옴
+			contents.getChildren().removeAll();
+			contents.getChildren().setAll(top50Page);
+			
+		//System.out.println(item_SigerManage.getText());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@FXML
+	public void periodPageChange(ActionEvent event) { //차트메뉴에서 Top50차트 클릭 했을때 페이지 전환 이벤트
+		try {
+			Parent top50Page = FXMLLoader.load(getClass().getResource("../view/chartmenu/main/ChartMenu.fxml")); //바뀔 화면을 가져옴
+			contents.getChildren().removeAll();
+			contents.getChildren().setAll(top50Page);
+			
+		//System.out.println(item_SigerManage.getText());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@FXML
+	public void musicvideoPageChange(ActionEvent event) { //차트메뉴에서 Top50차트 클릭 했을때 페이지 전환 이벤트
+		try {
+			Parent top50Page = FXMLLoader.load(getClass().getResource("../view/chartmenu/main/ChartMenu.fxml")); //바뀔 화면을 가져옴
+			contents.getChildren().removeAll();
+			contents.getChildren().setAll(top50Page);
+			
+		//System.out.println(item_SigerManage.getText());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
