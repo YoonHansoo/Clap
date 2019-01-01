a6624f089a6383010df662bf5bc6fa50b63bd25a
201809_JMK <201809_JMK@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-01 06:03:45 +0000

    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51641 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/view/chartmenu/main/ChartMenuController.java
+++ Clap_Client/src/kr/or/ddit/clap/view/chartmenu/main/ChartMenuController.java
@@ -4,13 +4,44 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import com.jfoenix.controls.JFXTabPane;
+
+import javafx.scene.control.SingleSelectionModel;
+import javafx.scene.control.Tab;
 
 public class ChartMenuController implements Initializable{
 	
 	public static int menuCount = 0;
+	
+	@FXML JFXTabPane tabPane_main;
+	@FXML Tab tab_top50;
+	@FXML Tab tab_genre;
+	@FXML Tab tab_period;
+	@FXML Tab tab_musicvideo;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		
+		SingleSelectionModel<Tab> selectionModel = tabPane_main.getSelectionModel();
+		
+		switch (menuCount) {
+		
+			case 0:
+				selectionModel.select(0);
+				break;
+			case 1:
+				selectionModel.select(1);
+				break;
+			case 2:
+				selectionModel.select(2);
+				break;
+			case 3:
+				selectionModel.select(3);
+				break;
+			default:
+				break;
+		}
 		
 	}
 	
