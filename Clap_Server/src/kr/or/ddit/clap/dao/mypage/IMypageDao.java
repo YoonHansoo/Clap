29982835f29a92ad9669f8fd3e616f36820437b4
201809_KHJ <201809_KHJ@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-01 09:13:23 +0000

    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51663 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Server/src/kr/or/ddit/clap/dao/mypage/IMypageDao.java
+++ Clap_Server/src/kr/or/ddit/clap/dao/mypage/IMypageDao.java
@@ -1,5 +1,11 @@
 package kr.or.ddit.clap.dao.mypage;
 
+import java.util.List;
+
+import kr.or.ddit.clap.vo.member.MemberVO;
+
 public interface IMypageDao {
 
+	public List<MemberVO> selectListAll();
+	
 }
--- Clap_Server/src/kr/or/ddit/clap/dao/mypage/MypageDaoImpl.java
+++ Clap_Server/src/kr/or/ddit/clap/dao/mypage/MypageDaoImpl.java
@@ -1,5 +1,53 @@
 package kr.or.ddit.clap.dao.mypage;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+
+import kr.or.ddit.clap.vo.music.MusicHistoryVO;
+
 public class MypageDaoImpl {
 
+	private SqlMapClient smc;
+	private static MypageDaoImpl dao; // Singleton 패턴
+
+	private MypageDaoImpl() {
+		Reader rd;
+		try {
+			rd = Resources.getResourceAsReader("SqlMapConfig.xml");
+			smc = SqlMapClientBuilder.buildSqlMapClient(rd);
+			rd.close();
+		} catch (IOException e) {
+			System.out.println("SqlMapClient객체 생성 실패!!");
+			e.printStackTrace();
+		}
+	}
+
+	public static MypageDaoImpl getInstance() { // Singleton 패턴
+		if (dao == null) {
+			dao = new MypageDaoImpl();
+		}
+		return dao;
+	}
+
+	public List<MusicHistoryVO> selectListAll() {
+		List<MusicHistoryVO> list = new ArrayList<MusicHistoryVO>();
+		try {
+
+			list = smc.queryForList("mypage.selectListAll");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+
+	
 }
