b416777633834a6299b5d2ed900ec706df3057fe
201809_JMK <201809_JMK@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-02 10:09:28 +0000

    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51835 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/view/chartmenu/top50/Top50RealTimeController.java
+++ Clap_Client/src/kr/or/ddit/clap/view/chartmenu/top50/Top50RealTimeController.java
@@ -5,11 +5,15 @@ import java.util.ResourceBundle;
 import com.jfoenix.controls.JFXCheckBox;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 /**
  * 
@@ -23,37 +27,50 @@ public class Top50RealTimeController implements Initializable{
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		
+		
 		VBox vbox = new VBox();
 		
-		for (int i = 0; i < 2; i++) {
+		for (int i = 0; i < 10; i++) {
 			// 파란색 라인 HBox 
-			HBox hLine = new HBox();
-			hLine.setPrefWidth(1004);
-			hLine.setPrefHeight(3);
-			hLine.setStyle("-fx-background-color: #70d3ff;");
+			HBox h_Line = new HBox();
+			h_Line.setPrefWidth(1004);
+			h_Line.setPrefHeight(3);
+			h_Line.setStyle("-fx-background-color: #70d3ff;");
 			
 				// 순위 곡정보 듣기 추가 담기 뮤비를 담는 전체 HBox 
-				HBox hTable = new HBox();
-				hTable.setPadding(new Insets(10,10,10,10));
-				hTable.setSpacing(10);
+				HBox h_Table = new HBox();
+				h_Table.setPadding(new Insets(10,10,10,10));
+				h_Table.setSpacing(10);
 				
 					// 체크박스,라벨,이미지뷰를 담는 HBox
-					HBox hCheckRankImg = new HBox();
-					hCheckRankImg.setPrefWidth(170);
-					hCheckRankImg.setPrefHeight(51);
-					hCheckRankImg.setMaxWidth(170);
+					HBox h_CheckRankImg = new HBox();
+					h_CheckRankImg.setAlignment(Pos.CENTER_LEFT);
+					h_CheckRankImg.setSpacing(10);
+					h_CheckRankImg.setPrefWidth(170);
+					h_CheckRankImg.setPrefHeight(51);
+					h_CheckRankImg.setMaxWidth(170);
 					
-					JFXCheckBox chbCheck = new JFXCheckBox();
-					chbCheck.setPrefWidth(30);
-					chbCheck.setPrefHeight(15);
-					chbCheck.setCheckedColor(Color.valueOf("#70d3ff"));
+					JFXCheckBox chb_Check = new JFXCheckBox();
+					chb_Check.setPrefWidth(30);
+					chb_Check.setPrefHeight(15);
+					chb_Check.setCheckedColor(Color.valueOf("#70d3ff"));
+					
+					Label la_Rank = new Label();
+					h_CheckRankImg.setMargin(la_Rank, new Insets(0,0,0,5));
+					la_Rank.setFont(Font.font("YDIYGO350", 18));
+					la_Rank.setText("" + (i+1));
+					
+					ImageView iv_album = new ImageView();
+					Image img_Path = new Image("http://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/081/142/924/81142924_1546233762478_1_600x600.JPG");
+					iv_album.setImage(img_Path);
 					
 			
 			
 			
-					hCheckRankImg.getChildren().addAll(chbCheck);
-				hTable.getChildren().add(hCheckRankImg);
-			vbox.getChildren().addAll(hLine,hTable);
+					h_CheckRankImg.getChildren().addAll(chb_Check,la_Rank,iv_album);
+				h_Table.getChildren().add(h_CheckRankImg);
+			vbox.getChildren().addAll(h_Line,h_Table);
 			
 		}
 		
