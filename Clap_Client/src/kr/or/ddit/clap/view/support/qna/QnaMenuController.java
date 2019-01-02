cc93aec81f162e19e9d6b86a97493dcd14234d8e
201809_LHH <201809_LHH@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-02 10:29:04 +0000

    QnaMenu 수정
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51847 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/view/support/qna/QnaMenuController.java
+++ Clap_Client/src/kr/or/ddit/clap/view/support/qna/QnaMenuController.java
@@ -1,15 +1,41 @@
+/**
+ *  문의사항 페이지를 출력하는 화면 controller
+ *  
+ *  @author hanhwa
+ */
 package kr.or.ddit.clap.view.support.qna;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import kr.or.ddit.clap.vo.support.QnaVO;
 
 public class QnaMenuController implements Initializable {
+	
+	@FXML
+	Pagination p_paging;
+	@FXML
+	TableView<QnaVO> tbl_qna;
+	@FXML
+	TableColumn<String , QnaVO> col_qnaNumber;
+	@FXML
+	TableColumn<String , QnaVO> col_qnaTitle;
+	@FXML
+	TableColumn<String , QnaVO> col_qnaDate;
+	@FXML
+	TableColumn<String , QnaVO> col_qnaViewCnt;
 
+	
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		
+		
 		
 	}
 
