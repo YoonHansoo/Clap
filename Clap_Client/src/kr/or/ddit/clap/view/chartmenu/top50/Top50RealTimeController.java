48996ed9c070ad42ca90bcc3d22b64271f646234
201809_JMK <201809_JMK@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-02 12:21:50 +0000

    테이블 형식으로 이제 나온다..
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51891 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/view/chartmenu/top50/Top50RealTimeController.java
+++ Clap_Client/src/kr/or/ddit/clap/view/chartmenu/top50/Top50RealTimeController.java
@@ -2,7 +2,11 @@ package kr.or.ddit.clap.view.chartmenu.top50;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -51,28 +55,126 @@ public class Top50RealTimeController implements Initializable{
 					h_CheckRankImg.setPrefHeight(51);
 					h_CheckRankImg.setMaxWidth(170);
 					
-					JFXCheckBox chb_Check = new JFXCheckBox();
-					chb_Check.setPrefWidth(30);
-					chb_Check.setPrefHeight(15);
-					chb_Check.setCheckedColor(Color.valueOf("#70d3ff"));
+						JFXCheckBox chb_Check = new JFXCheckBox();
+						chb_Check.setPrefWidth(30);
+						chb_Check.setPrefHeight(15);
+						chb_Check.setCheckedColor(Color.valueOf("#70d3ff"));
+						
+						// 순위를 나타내는 Label
+						Label la_Rank = new Label();
+						h_CheckRankImg.setMargin(la_Rank, new Insets(0,0,0,5));
+						la_Rank.setFont(Font.font("YDIYGO350", 18));
+						la_Rank.setPrefWidth(23);
+						la_Rank.setPrefHeight(23);
+						la_Rank.setText("" + (i+1));
+						
+						// 앨범이미지를 표시하는 ImageView
+						ImageView iv_Album = new ImageView();
+						h_CheckRankImg.setMargin(iv_Album, new Insets(0,0,0,20));
+						Image img_Path = new Image("http://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/081/142/924/81142924_1546233762478_1_600x600.JPG");
+						iv_Album.setImage(img_Path);
+						iv_Album.setFitWidth(50);
+						iv_Album.setFitHeight(50);
+						
 					
-					Label la_Rank = new Label();
-					h_CheckRankImg.setMargin(la_Rank, new Insets(0,0,0,5));
-					la_Rank.setFont(Font.font("YDIYGO350", 18));
-					la_Rank.setText("" + (i+1));
+					// 곡제목 및 아티스트 VBox를 담는 HBox
+					HBox h_MusicInfo = new HBox();
+					h_Table.setMargin(h_MusicInfo, new Insets(0,0,0,60));
+					h_MusicInfo.setAlignment(Pos.CENTER_LEFT);
+					h_MusicInfo.setPrefWidth(400);
+					h_MusicInfo.setPrefHeight(50);
+						
+						// 곡제목 및 아티스트명을 담는 VBox
+						VBox v_MusicInfo = new VBox();
+						v_MusicInfo.setAlignment(Pos.CENTER_LEFT);
+						v_MusicInfo.setSpacing(5);
+
+							// 곡제목을 담당하는 라벨
+							Label la_MusicName = new Label();
+							la_MusicName.setFont(Font.font("YDIYGO350", 25));
+							la_MusicName.setText("곡제목");
+							
+							// 곡제목을 담당하는 라벨
+							Label la_SingerName = new Label();
+							la_SingerName.setFont(Font.font("YDIYGO350", 18));
+							la_SingerName.setText("아티스트명");
+							
+							
+					// 듣기 추가 담기 뮤비를 담는 HBox
+					HBox h_MusicBtn = new HBox();
+					h_MusicBtn.setAlignment(Pos.CENTER_LEFT);
+					h_MusicBtn.setPrefWidth(170);
+					h_MusicBtn.setPrefHeight(50);
+					h_MusicBtn.setMaxWidth(170);
+					h_MusicBtn.setSpacing(10);
 					
-					ImageView iv_album = new ImageView();
-					Image img_Path = new Image("http://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/081/142/924/81142924_1546233762478_1_600x600.JPG");
-					iv_album.setImage(img_Path);
-					iv_album.setFitWidth(50);
-					iv_album.setFitHeight(50);
-					h_CheckRankImg.setMargin(iv_album, new Insets(0,0,0,20));
+						// 듣기 버튼
+						JFXButton btn_Play = new JFXButton();
+						btn_Play.setRipplerFill(Color.valueOf("#70d3ff"));
+						btn_Play.setAlignment(Pos.CENTER_LEFT);
+						btn_Play.setPrefWidth(30);
+						btn_Play.setPrefHeight(46);
+							
+							// 듣기 아이콘
+							FontAwesomeIcon icon_Play = new FontAwesomeIcon();
+							icon_Play.setIconName("PLAY_CIRCLE_ALT");
+							icon_Play.setFill(Color.valueOf("#318dc5"));
+							icon_Play.setSize("1cm");
+							btn_Play.setGraphic(icon_Play);
+							
+						// 추가 버튼
+						JFXButton btn_Add = new JFXButton();
+						btn_Add.setRipplerFill(Color.valueOf("#70d3ff"));
+						btn_Add.setAlignment(Pos.CENTER_LEFT);
+						btn_Add.setPrefWidth(30);
+						btn_Add.setPrefHeight(46);
+							
+							// 추가 아이콘
+							FontAwesomeIcon icon_Add = new FontAwesomeIcon();
+							icon_Add.setIconName("PLUS");
+							icon_Add.setFill(Color.valueOf("#318dc5"));
+							icon_Add.setSize("1cm");
+							btn_Add.setGraphic(icon_Add);
+							
+						// 담기 버튼
+						JFXButton btn_Put = new JFXButton();
+						btn_Put.setRipplerFill(Color.valueOf("#70d3ff"));
+						btn_Put.setAlignment(Pos.CENTER_LEFT);
+						btn_Put.setPrefWidth(30);
+						btn_Put.setPrefHeight(46);
+							
+							// 담기 아이콘
+							FontAwesomeIcon icon_Put = new FontAwesomeIcon();
+							icon_Put.setIconName("FOLDER_ALT");
+							icon_Put.setFill(Color.valueOf("#318dc5"));
+							icon_Put.setSize("1cm");
+							btn_Put.setGraphic(icon_Put);
+						
+						// 담기 버튼
+						JFXButton btn_Movie = new JFXButton();
+						btn_Movie.setRipplerFill(Color.valueOf("#70d3ff"));
+						btn_Movie.setAlignment(Pos.CENTER_LEFT);
+						btn_Movie.setPrefWidth(30);
+						btn_Movie.setPrefHeight(46);
+							
+							// 담기 아이콘
+							FontAwesomeIcon icon_Movie = new FontAwesomeIcon();
+							icon_Movie.setIconName("VIMEO_SQUARE");
+							icon_Movie.setFill(Color.valueOf("#318dc5"));
+							icon_Movie.setSize("1cm");
+							btn_Movie.setGraphic(icon_Movie);
+						
+						
+							
+							
+							
+							
 					
-			
-			
-			
-					h_CheckRankImg.getChildren().addAll(chb_Check,la_Rank,iv_album);
-				h_Table.getChildren().add(h_CheckRankImg);
+						v_MusicInfo.getChildren().addAll(la_MusicName,la_SingerName);
+					h_MusicBtn.getChildren().addAll(btn_Play,btn_Add,btn_Put,btn_Movie);
+					h_MusicInfo.getChildren().add(v_MusicInfo);
+					h_CheckRankImg.getChildren().addAll(chb_Check,la_Rank,iv_Album);
+				h_Table.getChildren().addAll(h_CheckRankImg,h_MusicInfo,h_MusicBtn);
 			vbox.getChildren().addAll(h_Line,h_Table);
 			
 		}
