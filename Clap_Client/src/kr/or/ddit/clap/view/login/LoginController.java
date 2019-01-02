1a0644b85fde41780232456fc9dbddd6ee9ec465
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-02 09:33:20 +0000

    login 수정
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51821 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/view/login/LoginController.java
+++ Clap_Client/src/kr/or/ddit/clap/view/login/LoginController.java
@@ -2,6 +2,10 @@ package kr.or.ddit.clap.view.login;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ResourceBundle;
@@ -9,10 +13,17 @@ import java.util.ResourceBundle;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
 import kr.or.ddit.clap.main.MusicMainController;
+import kr.or.ddit.clap.service.login.ILoginService;
+import kr.or.ddit.clap.service.singer.ISingerService;
 import kr.or.ddit.clap.view.join.AES256Util;
+import kr.or.ddit.clap.vo.member.MemberVO;
 
 /**
  * 로그인창 컨트롤러.
@@ -22,16 +33,25 @@ import kr.or.ddit.clap.view.join.AES256Util;
 public class LoginController implements Initializable{
 	
 	MusicMainController mmc = new MusicMainController();
+	private ILoginService ils;
+	private Registry reg;
 	
 	@FXML JFXTextField txt_id;
 	@FXML JFXPasswordField txt_pw;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+		try {
+			reg = LocateRegistry.getRegistry("localhost", 8888);
+			ils = (ILoginService) reg.lookup("login");
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void login() throws UnsupportedEncodingException, NoSuchAlgorithmException, GeneralSecurityException {
+	public void login() throws UnsupportedEncodingException, NoSuchAlgorithmException, GeneralSecurityException, RemoteException {
 		AES256Util aes = new AES256Util();
 		
 		String encryptedPw = ""; // 암호화된 pw
@@ -41,9 +61,10 @@ public class LoginController implements Initializable{
 		decryptedPw = aes.decrypt(encryptedPw);
 	
 		// 아이디 확인
+		MemberVO vo = ils.select(txt_id.getText());
 		
 		// 아이디가 존재하지 않을 때
-		if(false) {
+		if(vo == null) {
 			System.out.println("존재하지 않는 ID입니다.");
 		}
 		
