490e8dbef474e42abc70328b62add060327a09c4
201809_LHH <201809_LHH@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-02 06:43:29 +0000

    QnaMenu
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51772 d808369f-3623-194b-b0f8-88541be8a525

---
+++ Clap_Client/src/kr/or/ddit/clap/view/support/qna/QnaMenuController.java
@@ -0,0 +1,16 @@
+package kr.or.ddit.clap.view.support.qna;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class QnaMenuController implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
