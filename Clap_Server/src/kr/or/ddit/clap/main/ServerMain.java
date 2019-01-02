2a144590aafd2c2f1288ca8710c2f5630a54ea9c
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-02 10:49:45 +0000

    login rebind 수정
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@51855 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Server/src/kr/or/ddit/clap/main/ServerMain.java
+++ Clap_Server/src/kr/or/ddit/clap/main/ServerMain.java
@@ -3,6 +3,8 @@ package kr.or.ddit.clap.main;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
+import kr.or.ddit.clap.service.login.ILoginService;
+import kr.or.ddit.clap.service.login.LoginServiceImpl;
 import kr.or.ddit.clap.service.mypage.IMypageService;
 import kr.or.ddit.clap.service.mypage.MypageServiceImpl;
 import kr.or.ddit.clap.service.singer.ISingerService;
@@ -15,11 +17,13 @@ public class ServerMain {
 //			IBoardService bsi = new BoardServiceImpl();
 			ISingerService ssi 	= SingerServiceImpl.getInstance();
 			IMypageService ms 	= MypageServiceImpl.getInstance(); //객체생성
+			ILoginService ils 	= LoginServiceImpl.getInstance();
 			
 			Registry reg = LocateRegistry.createRegistry(8888);
 			
 			reg.rebind("singer", ssi);
 			reg.rebind("mypage", ms);
+			reg.rebind("login", ils);
 			  System.out.println("clap server  is running...");
 			  
 		} catch (Exception e) {
