7f607fe89690c5377479c71457f7ba91f85e9ec4
201809_KHJ <201809_KHJ@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-03 08:21:54 +0000

    서비스
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@52111 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/service/mypage/IMypageService.java
+++ Clap_Client/src/kr/or/ddit/clap/service/mypage/IMypageService.java
@@ -2,8 +2,10 @@ package kr.or.ddit.clap.service.mypage;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 import kr.or.ddit.clap.vo.member.MemberVO;
+import kr.or.ddit.clap.vo.music.MusicReviewVO;
 
 public interface IMypageService  extends Remote {
 	public MemberVO select(MemberVO vo) throws RemoteException;
@@ -11,4 +13,5 @@ public interface IMypageService  extends Remote {
 	public int  updateEmail(MemberVO vo) throws RemoteException;
 	public int  updatePw(MemberVO vo) throws RemoteException;
 	public int  updateDelTF(MemberVO vo) throws RemoteException;
+	public List<MusicReviewVO> selectReview(MemberVO vo) throws RemoteException;
 }
