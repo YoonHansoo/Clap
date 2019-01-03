196751be3cc671b1a5d66f2085f6804431df5182
201809_LHH <201809_LHH@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-03 05:40:06 +0000

    Qna 수정
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@52022 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/view/support/qna/QnaMenuController.java
+++ Clap_Client/src/kr/or/ddit/clap/view/support/qna/QnaMenuController.java
@@ -22,34 +22,34 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Pagination;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
 import kr.or.ddit.clap.service.qna.IQnaService;
 import kr.or.ddit.clap.vo.support.QnaVO;
 
 public class QnaMenuController implements Initializable {
-	
+
 	@FXML
 	Pagination p_paging;
 	@FXML
 	JFXTreeTableView<QnaVO> tbl_qna;
 	@FXML
-	TableColumn<QnaVO, String> col_qnaNumber;
+	TreeTableColumn<QnaVO, String> col_qnaNumber;
 	@FXML
-	TableColumn<QnaVO, String> col_qnaTitle;
+	TreeTableColumn<QnaVO, String> col_qnaTitle;
 	@FXML
-	TableColumn<QnaVO, String> col_qnaDate;
+	TreeTableColumn<QnaVO, String> col_qnaDate;
 	@FXML
-	TableColumn<QnaVO, String> col_qnaViewCnt;
+	TreeTableColumn<QnaVO, String> col_qnaViewCnt;
 
 	private Registry reg;
 	private IQnaService iqs;
-	private ObservableList<QnaVO> qnaList;
-	
-	
+	private ObservableList<QnaVO> qnaList, currentqnaList;
+	private int from, to, itemsForPage, totalPageCnt;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+
 		try {
 			reg = LocateRegistry.getRegistry("localhost", 8888);
 			iqs = (IQnaService) reg.lookup("qna");
@@ -58,25 +58,61 @@ public class QnaMenuController implements Initializable {
 		} catch (NotBoundException e) {
 			e.printStackTrace();
 		}
-		
-		col_qnaDate.setCellValueFactory(param ->new SimpleStringProperty(param.getValue().getQna_indate()));
-		col_qnaNumber.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getQna_no()));
-		col_qnaTitle.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getQna_title()));
-		col_qnaViewCnt.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getQna_view_cnt()));
-		
+
+		col_qnaDate.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getValue().getQna_indate()));
+		col_qnaNumber.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getValue().getQna_no()));
+		col_qnaTitle.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getValue().getQna_title()));
+		col_qnaViewCnt.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getValue().getQna_view_cnt()));
+
 		try {
 			qnaList = FXCollections.observableArrayList(iqs.selectListAll());
 		} catch (RemoteException e) {
 			System.out.println("에러");
 			e.printStackTrace();
 		}
-		
-		//데이터 삽입
+
+		// 데이터 삽입
 		TreeItem<QnaVO> root = new RecursiveTreeItem<>(qnaList, RecursiveTreeObject::getChildren);
 		tbl_qna.setRoot(root);
 		tbl_qna.setShowRoot(false);
-		
-		
+
+		itemsForPage = 10; // 한페이지 보여줄 항목 수 설정
+
+		paging();
+
+	}
+
+	// 페이징 메서드
+	private void paging() {
+		totalPageCnt = qnaList.size() % itemsForPage == 0 ? qnaList.size() / itemsForPage
+				: qnaList.size() / itemsForPage + 1;
+
+		p_paging.setPageCount(totalPageCnt); // 전체 페이지 수 설정
+
+		p_paging.setPageFactory((Integer pageIndex) -> {
+
+			from = pageIndex * itemsForPage;
+			to = from + itemsForPage - 1;
+
+			TreeItem<QnaVO> root = new RecursiveTreeItem<>(getTableViewData(from, to),
+					RecursiveTreeObject::getChildren);
+			tbl_qna.setRoot(root);
+			tbl_qna.setShowRoot(false);
+			return tbl_qna;
+		});
+	}
+
+	// 페이징에 맞는 데이터를 가져옴
+	private ObservableList<QnaVO> getTableViewData(int from, int to) {
+
+		currentqnaList = FXCollections.observableArrayList(); //
+		int totSize = qnaList.size();
+		for (int i = from; i <= to && i < totSize; i++) {
+
+			currentqnaList.add(qnaList.get(i));
+		}
+
+		return currentqnaList;
 	}
 
 }
