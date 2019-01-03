9fe7bdf6fef8cc6d198f00961046abd4dec19e55
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-03 05:28:12 +0000

    로그인 수정
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@52002 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/service/login/ILoginService.java
+++ Clap_Client/src/kr/or/ddit/clap/service/login/ILoginService.java
@@ -2,9 +2,6 @@ package kr.or.ddit.clap.service.login;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.List;
-
-import kr.or.ddit.clap.vo.member.MemberVO;
 
 public interface ILoginService extends Remote{
 	public Boolean idCheck(String id) throws RemoteException;
