671ea39a28af52c90594e2f88585ddadacb84d99
201809_JMK <201809_JMK@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-03 06:16:46 +0000

    스타일 시트 추가
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@52042 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/view/chartmenu/top50/Top50RealTimeController.java
+++ Clap_Client/src/kr/or/ddit/clap/view/chartmenu/top50/Top50RealTimeController.java
@@ -37,143 +37,117 @@ public class Top50RealTimeController implements Initializable{
 		for (int i = 0; i < 20; i++) {
 			// 파란색 라인 HBox 
 			HBox h_Line = new HBox();
-			h_Line.setPrefWidth(1004);
+			vbox.setMargin(h_Line, new Insets(0,0,0,90));
+			h_Line.setPrefWidth(710);
 			h_Line.setPrefHeight(3);
-			h_Line.setStyle("-fx-background-color: #70d3ff;");
+			h_Line.setStyle("-fx-background-color: #090948;");
 			
-				// 순위 곡정보 듣기 추가 담기 뮤비를 담는 전체 HBox 
-				HBox h_Table = new HBox();
-				h_Table.setPadding(new Insets(10,10,10,10));
-				h_Table.setSpacing(10);
+			// 전체 HBox 
+			HBox h_Table = new HBox();
+			vbox.setMargin(h_Table, new Insets(0,0,0,80));
+			h_Table.setAlignment(Pos.CENTER_LEFT);
+			h_Table.setPadding(new Insets(10,10,10,10));
+			h_Table.setSpacing(10);
+			
+				JFXCheckBox chb_Check = new JFXCheckBox();
+				chb_Check.setPrefWidth(30);
+				chb_Check.setPrefHeight(15);
+				chb_Check.setCheckedColor(Color.valueOf("#70d3ff"));
 				
-					// 체크박스,라벨,이미지뷰를 담는 HBox
-					HBox h_CheckRankImg = new HBox();
-					h_CheckRankImg.setAlignment(Pos.CENTER_LEFT);
-					h_CheckRankImg.setSpacing(10);
-					h_CheckRankImg.setPrefWidth(170);
-					h_CheckRankImg.setPrefHeight(51);
-					h_CheckRankImg.setMaxWidth(170);
+				// 순위를 나타내는 Label
+				Label la_Rank = new Label();
+				la_Rank.setFont(Font.font("-윤고딕350", 12));
+				la_Rank.setPrefWidth(23);
+				la_Rank.setPrefHeight(23);
+				la_Rank.setText("" + (i+1));
+				
+				// 앨범이미지를 표시하는 ImageView
+				ImageView iv_Album = new ImageView();
+				Image img_Path = new Image("http://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/081/142/924/81142924_1546233762478_1_600x600.JPG");
+				iv_Album.setImage(img_Path);
+				iv_Album.setFitWidth(50);
+				iv_Album.setFitHeight(50);
 					
-						JFXCheckBox chb_Check = new JFXCheckBox();
-						chb_Check.setPrefWidth(30);
-						chb_Check.setPrefHeight(15);
-						chb_Check.setCheckedColor(Color.valueOf("#70d3ff"));
-						
-						// 순위를 나타내는 Label
-						Label la_Rank = new Label();
-						h_CheckRankImg.setMargin(la_Rank, new Insets(0,0,0,5));
-						la_Rank.setFont(Font.font("YDIYGO350", 12));
-						la_Rank.setPrefWidth(23);
-						la_Rank.setPrefHeight(23);
-						la_Rank.setText("" + (i+1));
-						
-						// 앨범이미지를 표시하는 ImageView
-						ImageView iv_Album = new ImageView();
-						h_CheckRankImg.setMargin(iv_Album, new Insets(0,0,0,20));
-						Image img_Path = new Image("http://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/081/142/924/81142924_1546233762478_1_600x600.JPG");
-						iv_Album.setImage(img_Path);
-						iv_Album.setFitWidth(50);
-						iv_Album.setFitHeight(50);
-						
-					
-					// 곡제목 및 아티스트 VBox를 담는 HBox
-					HBox h_MusicInfo = new HBox();
-					h_Table.setMargin(h_MusicInfo, new Insets(0,0,0,60));
-					h_MusicInfo.setAlignment(Pos.CENTER_LEFT);
-					h_MusicInfo.setPrefWidth(400);
-					h_MusicInfo.setPrefHeight(50);
-						
-						// 곡제목 및 아티스트명을 담는 VBox
-						VBox v_MusicInfo = new VBox();
-						v_MusicInfo.setAlignment(Pos.CENTER_LEFT);
-						v_MusicInfo.setSpacing(5);
+				// 곡제목 및 아티스트명을 담는 VBox
+				VBox v_MusicInfo = new VBox();
+				v_MusicInfo.setAlignment(Pos.CENTER_LEFT);
+				v_MusicInfo.setSpacing(5);
 
-							// 곡제목을 담당하는 라벨
-							Label la_MusicName = new Label();
-							la_MusicName.setFont(Font.font("YDIYGO350", 15));
-							la_MusicName.setText("곡제목");
-							
-							// 곡제목을 담당하는 라벨
-							Label la_SingerName = new Label();
-							la_SingerName.setFont(Font.font("YDIYGO320", 12));
-							la_SingerName.setText("아티스트명");
-							
-							
-					// 듣기 추가 담기 뮤비를 담는 HBox
-					HBox h_MusicBtn = new HBox();
-					h_MusicBtn.setAlignment(Pos.CENTER_LEFT);
-					h_MusicBtn.setPrefWidth(170);
-					h_MusicBtn.setPrefHeight(50);
-					h_MusicBtn.setMaxWidth(170);
-					h_MusicBtn.setSpacing(10);
+					// 곡제목을 담당하는 라벨
+					Label la_MusicName = new Label();
+					la_MusicName.setFont(Font.font("-윤고딕350", 12));
+					la_MusicName.setText("곡제목");
 					
-						// 듣기 버튼
-						JFXButton btn_Play = new JFXButton();
-						btn_Play.setRipplerFill(Color.valueOf("#70d3ff"));
-						btn_Play.setAlignment(Pos.CENTER_LEFT);
-						btn_Play.setPrefWidth(30);
-						btn_Play.setPrefHeight(46);
-							
-							// 듣기 아이콘
-							FontAwesomeIcon icon_Play = new FontAwesomeIcon();
-							icon_Play.setIconName("PLAY_CIRCLE_ALT");
-							icon_Play.setFill(Color.valueOf("#318dc5"));
-							icon_Play.setSize("1cm");
-							btn_Play.setGraphic(icon_Play);
-							
-						// 추가 버튼
-						JFXButton btn_Add = new JFXButton();
-						btn_Add.setRipplerFill(Color.valueOf("#70d3ff"));
-						btn_Add.setAlignment(Pos.CENTER_LEFT);
-						btn_Add.setPrefWidth(30);
-						btn_Add.setPrefHeight(46);
-							
-							// 추가 아이콘
-							FontAwesomeIcon icon_Add = new FontAwesomeIcon();
-							icon_Add.setIconName("PLUS");
-							icon_Add.setFill(Color.valueOf("#318dc5"));
-							icon_Add.setSize("1cm");
-							btn_Add.setGraphic(icon_Add);
-							
-						// 담기 버튼
-						JFXButton btn_Put = new JFXButton();
-						btn_Put.setRipplerFill(Color.valueOf("#70d3ff"));
-						btn_Put.setAlignment(Pos.CENTER_LEFT);
-						btn_Put.setPrefWidth(30);
-						btn_Put.setPrefHeight(46);
-							
-							// 담기 아이콘
-							FontAwesomeIcon icon_Put = new FontAwesomeIcon();
-							icon_Put.setIconName("FOLDER_ALT");
-							icon_Put.setFill(Color.valueOf("#318dc5"));
-							icon_Put.setSize("1cm");
-							btn_Put.setGraphic(icon_Put);
+					// 곡제목을 담당하는 라벨
+					Label la_SingerName = new Label();
+					la_SingerName.setFont(Font.font("-윤고딕330", 12));
+					la_SingerName.setText("아티스트명");
 						
-						// 담기 버튼
-						JFXButton btn_Movie = new JFXButton();
-						btn_Movie.setRipplerFill(Color.valueOf("#70d3ff"));
-						btn_Movie.setAlignment(Pos.CENTER_LEFT);
-						btn_Movie.setPrefWidth(30);
-						btn_Movie.setPrefHeight(46);
-							
-							// 담기 아이콘
-							FontAwesomeIcon icon_Movie = new FontAwesomeIcon();
-							icon_Movie.setIconName("VIMEO_SQUARE");
-							icon_Movie.setFill(Color.valueOf("#318dc5"));
-							icon_Movie.setSize("1cm");
-							btn_Movie.setGraphic(icon_Movie);
-						
-						
-							
-							
-							
-							
+				// 듣기 버튼
+				JFXButton btn_Play = new JFXButton();
+				h_Table.setMargin(btn_Play, new Insets(0,0,0,285));
+				btn_Play.setRipplerFill(Color.valueOf("#70d3ff"));
+				btn_Play.setAlignment(Pos.CENTER_LEFT);
+				btn_Play.setPrefWidth(30);
+				btn_Play.setPrefHeight(46);
 					
-						v_MusicInfo.getChildren().addAll(la_MusicName,la_SingerName);
-					h_MusicBtn.getChildren().addAll(btn_Play,btn_Add,btn_Put,btn_Movie);
-					h_MusicInfo.getChildren().add(v_MusicInfo);
-					h_CheckRankImg.getChildren().addAll(chb_Check,la_Rank,iv_Album);
-				h_Table.getChildren().addAll(h_CheckRankImg,h_MusicInfo,h_MusicBtn);
+					// 듣기 아이콘
+					FontAwesomeIcon icon_Play = new FontAwesomeIcon();
+					icon_Play.setIconName("PLAY_CIRCLE_ALT");
+					icon_Play.setFill(Color.valueOf("#318dc5"));
+					icon_Play.setSize("30");
+					btn_Play.setGraphic(icon_Play);
+					
+				// 추가 버튼
+				JFXButton btn_Add = new JFXButton();
+				btn_Add.setRipplerFill(Color.valueOf("#70d3ff"));
+				btn_Add.setAlignment(Pos.CENTER_LEFT);
+				btn_Add.setPrefWidth(30);
+				btn_Add.setPrefHeight(46);
+					
+					// 추가 아이콘
+					FontAwesomeIcon icon_Add = new FontAwesomeIcon();
+					icon_Add.setIconName("PLUS");
+					icon_Add.setFill(Color.valueOf("#318dc5"));
+					icon_Add.setSize("30");
+					btn_Add.setGraphic(icon_Add);
+					
+				// 담기 버튼
+				JFXButton btn_Put = new JFXButton();
+				btn_Put.setRipplerFill(Color.valueOf("#70d3ff"));
+				btn_Put.setAlignment(Pos.CENTER_LEFT);
+				btn_Put.setPrefWidth(30);
+				btn_Put.setPrefHeight(46);
+					
+					// 담기 아이콘
+					FontAwesomeIcon icon_Put = new FontAwesomeIcon();
+					icon_Put.setIconName("FOLDER_ALT");
+					icon_Put.setFill(Color.valueOf("#318dc5"));
+					icon_Put.setSize("30");
+					btn_Put.setGraphic(icon_Put);
+				
+				// 담기 버튼
+				JFXButton btn_Movie = new JFXButton();
+				btn_Movie.setRipplerFill(Color.valueOf("#70d3ff"));
+				btn_Movie.setAlignment(Pos.CENTER_LEFT);
+				btn_Movie.setPrefWidth(30);
+				btn_Movie.setPrefHeight(46);
+					
+					// 담기 아이콘
+					FontAwesomeIcon icon_Movie = new FontAwesomeIcon();
+					icon_Movie.setIconName("VIMEO_SQUARE");
+					icon_Movie.setFill(Color.valueOf("#318dc5"));
+					icon_Movie.setSize("30");
+					btn_Movie.setGraphic(icon_Movie);
+					
+					
+						
+						
+						
+						
+				
+				v_MusicInfo.getChildren().addAll(la_MusicName,la_SingerName);
+			h_Table.getChildren().addAll(chb_Check,la_Rank,iv_Album,v_MusicInfo,btn_Play,btn_Add,btn_Put,btn_Movie);
 			vbox.getChildren().addAll(h_Line,h_Table);
 			
 		}
