168ab801eafcdafbd2755acd4ea6cf0914048c82
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-03 05:28:25 +0000

    로그인 수정
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@52003 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Server/src/kr/or/ddit/clap/dao/login/ILoginDao.java
+++ Clap_Server/src/kr/or/ddit/clap/dao/login/ILoginDao.java
@@ -1,7 +1,5 @@
 package kr.or.ddit.clap.dao.login;
 
-import kr.or.ddit.clap.vo.member.MemberVO;
-
 public interface ILoginDao {
-	public MemberVO select(String id);
+	public Boolean idCheck(String id);
 }
