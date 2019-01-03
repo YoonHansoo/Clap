fe92d04dddf6166182de198f86a1f8a668cc3a51
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-03 06:55:07 +0000

    회원가입 수정
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@52058 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/view/join/JoinController.java
+++ Clap_Client/src/kr/or/ddit/clap/view/join/JoinController.java
@@ -4,13 +4,17 @@ import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextField;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleGroup;
 import kr.or.ddit.clap.vo.member.MemberVO;
 
 /**
@@ -26,11 +30,22 @@ public class JoinController implements Initializable{
 	@FXML JFXTextField txt_birth;
 	@FXML JFXTextField txt_tel;
 	@FXML JFXTextField txt_email;
+	@FXML JFXTextField txt_name;
 	
 	@FXML JFXButton btn_ok;
+	@FXML JFXRadioButton radio_m;
+	@FXML JFXRadioButton radio_f;
+	
+	ToggleGroup group = new ToggleGroup();
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		radio_m.setToggleGroup(group);
+		radio_m.setUserData("m");
+		radio_m.setSelected(true);
+		
+		radio_f.setToggleGroup(group);
+		radio_f.setUserData("f");
 		
 	}
 
@@ -44,6 +59,11 @@ public class JoinController implements Initializable{
 		String encryptedPwCheck = aes.encrypt(txt_pwCheck.getText());
 		String decryptedPwCheck = aes.decrypt(encryptedPwCheck);
 		
+		String gender = group.getSelectedToggle().getUserData().toString();
+		
+		Date now = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yy/MM/dd");
+		
 		// 아이디 유효성 검사
 		
 		// 아이디 중복확인
@@ -54,28 +74,20 @@ public class JoinController implements Initializable{
 		
 		vo.setMem_id(txt_id.getText());
 		vo.setMem_pw(encryptedPw);
-		vo.setMem_name(txt_id.getText()); // 일단 아이디로.
+		vo.setMem_name(txt_name.getText());
 		vo.setMem_email(txt_email.getText());
 		vo.setMem_bir(txt_birth.getText());
 
-		vo.setMem_id(txt_id.getText());
-		vo.setMem_id(txt_id.getText());
-		vo.setMem_id(txt_id.getText());
-		vo.setMem_id(txt_id.getText());
-		vo.setMem_id(txt_id.getText());
+		vo.setMem_gender(gender);
+		vo.setMem_tel(txt_tel.getText());
+		vo.setMem_grade("일반");
+		vo.setMem_auth("f");
+		vo.setMem_indate(sdf.format(now));
 
-		vo.setMem_id(txt_id.getText());
-		vo.setMem_id(txt_id.getText());
+		vo.setMem_blacklist_tf("f");
+		vo.setMem_del_tf("f");
 		
 		System.out.println("아이디:"+txt_id.getText());
-		System.out.println("비번:"+txt_pw.getText());
-		System.out.println("비번확인:"+txt_pwCheck.getText());
-		System.out.println("암호화 비번:"+encryptedPw);
-		System.out.println("암호화 비번확인:"+encryptedPwCheck);
-		
-		System.out.println("생일:"+txt_birth.getText());
-		System.out.println("번호:"+txt_tel.getText());
-		System.out.println("이메일:"+txt_email.getText());
 	}
 	
 }
