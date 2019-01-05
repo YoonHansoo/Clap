624827a48559e2665b513046d2aeb0d0d5752fef
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-05 07:52:40 +0000

    들여쓰기 수정
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@52655 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/main/MusicMainController.java
+++ Clap_Client/src/kr/or/ddit/clap/main/MusicMainController.java
@@ -27,171 +27,181 @@ import com.jfoenix.controls.JFXButton;
 
 /**
  * 메인화면의 fxml 컨트롤러.
+ * 
  * @author Kyunghun
  *
  */
-public class MusicMainController implements Initializable{
-   
-   @FXML VBox vbox;
-   
-   static Stage buyTicketDialog = new Stage(StageStyle.DECORATED);
-   @FXML AnchorPane menu;
-   @FXML AnchorPane contents;
-   @FXML FontAwesomeIcon icon_firstPage;
-   @FXML HBox mem_menu;
+public class MusicMainController implements Initializable {
 
-   @FXML public JFXButton btn_login;
-   @FXML public JFXButton btn_logout;
-   @FXML public JFXButton btn_join;
-   @FXML public JFXButton btn_mem;
-   @FXML Menu menu_admin;
-   @FXML Label lb_id;
-   @FXML ImageView mem_img;
-   
-   LoginSession ls = new LoginSession();
-   
-   @Override
-   public void initialize(URL location, ResourceBundle resources) {
-	   System.out.println();
-	   if(ls.session!=null) {
-		   System.out.println(ls.session.getMem_name());
-		   System.out.println(ls.session.getMem_auth());
-	   }
-      if(ls.session == null) {
-         mem_menu.setVisible(false);
-         btn_join.setVisible(true);
-         btn_login.setVisible(true);         
-      }else {
-    	  System.out.println(ls.session.getMem_id() + ls.session.getMem_auth());
-    	  if(ls.session.getMem_auth().equals("t")) { //관리자 일 때 관리자모드 버튼 활성화
-    		  menu_admin.setVisible(true);
-    	  }
-    	  else {
-    		  menu_admin.setVisible(false);         //일반사용자일경우( 관리자로 로그인 후 사용자로 로그인 했을 경우를 대비해서만들었음
-    	  }
-         System.out.println("not null "+ls.session.getMem_id());
-         mem_menu.setVisible(true);
-         btn_join.setVisible(false);
-         btn_login.setVisible(false);
-         
-         lb_id.setText(ls.session.getMem_id()+"님");
-         
-         Image img = new Image(getClass().getResourceAsStream("../../../../../people_small.png"));
-         mem_img.setImage(img);
+	@FXML
+	VBox vbox;
 
-      }
-   }
-   
-   @FXML
-   public void login() throws IOException {
-	   try {
-	      Parent root = FXMLLoader.load(getClass().getResource("../view/login/Login.fxml"));
-	      contents.getChildren().removeAll();
-	      contents.getChildren().setAll(root);
-	   } catch (IOException e) {
-	      e.printStackTrace();
-	   }
-   }
-   
-   @FXML
-   public void logout() throws IOException {
-	   System.out.println("로그아웃처리");
-	   ls.session = null;
-	   System.out.println(ls.session);
-	   firstPage();
-   }
-   
-   @FXML
-   public void join() throws IOException {
-	   try {
+	static Stage buyTicketDialog = new Stage(StageStyle.DECORATED);
+	@FXML
+	AnchorPane menu;
+	@FXML
+	AnchorPane contents;
+	@FXML
+	FontAwesomeIcon icon_firstPage;
+	@FXML
+	HBox mem_menu;
+
+	@FXML
+	public JFXButton btn_login;
+	@FXML
+	public JFXButton btn_logout;
+	@FXML
+	public JFXButton btn_join;
+	@FXML
+	public JFXButton btn_mem;
+	@FXML
+	Menu menu_admin;
+	@FXML
+	Label lb_id;
+	@FXML
+	ImageView mem_img;
+
+	LoginSession ls = new LoginSession();
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		System.out.println();
+		if (ls.session != null) {
+			System.out.println(ls.session.getMem_name());
+			System.out.println(ls.session.getMem_auth());
+		}
+		if (ls.session == null) {
+			mem_menu.setVisible(false);
+			btn_join.setVisible(true);
+			btn_login.setVisible(true);
+		} else {
+			System.out.println(ls.session.getMem_id() + ls.session.getMem_auth());
+			if (ls.session.getMem_auth().equals("t")) { // 관리자 일 때 관리자모드 버튼 활성화
+				menu_admin.setVisible(true);
+			} else {
+				menu_admin.setVisible(false); // 일반사용자일경우( 관리자로 로그인 후 사용자로 로그인 했을 경우를 대비해서만들었음
+			}
+			System.out.println("not null " + ls.session.getMem_id());
+			mem_menu.setVisible(true);
+			btn_join.setVisible(false);
+			btn_login.setVisible(false);
+
+			lb_id.setText(ls.session.getMem_id() + "님");
+
+			Image img = new Image(getClass().getResourceAsStream("../../../../../people_small.png"));
+			mem_img.setImage(img);
+
+		}
+	}
+
+	@FXML
+	public void login() throws IOException {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("../view/login/Login.fxml"));
+			contents.getChildren().removeAll();
+			contents.getChildren().setAll(root);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	public void logout() throws IOException {
+		System.out.println("로그아웃처리");
+		ls.session = null;
+		System.out.println(ls.session);
+		firstPage();
+	}
+
+	@FXML
+	public void join() throws IOException {
+		try {
 			Parent root = FXMLLoader.load(getClass().getResource("../view/join/Join.fxml"));
 			contents.getChildren().removeAll();
 			contents.getChildren().setAll(root);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-   }
+	}
 
-   @FXML
-   public void mypage() {
-      // 로그인 후 회원정보버튼(btn_mem) 눌렀을때 마이페이지로 이동.
-      System.out.println("마이페이지 출력");
-      
-      try {
-         Parent root = FXMLLoader.load(getClass().getResource("../view/member/mypage/Mypage.fxml"));
-         contents.getChildren().removeAll();
-         contents.getChildren().setAll(root);
-      } catch (IOException e) {
-         e.printStackTrace();
-      }
-   }
-   
-   @FXML
-   public void buyTicket() throws IOException {
-      login_PageLoad();
-   }
-   
-   
-   @FXML
-   public void qna() { //고객센터 - 문의사항
+	@FXML
+	public void mypage() {
+		// 로그인 후 회원정보버튼(btn_mem) 눌렀을때 마이페이지로 이동.
+		System.out.println("마이페이지 출력");
 
-      try {
-         Parent qna = FXMLLoader.load(getClass().getResource("../view/support/qna/QnaMenuList.fxml"));
-         contents.getChildren().removeAll();
-         contents.getChildren().setAll(qna);
-      } catch (IOException e) {
-         e.printStackTrace();
-      }
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("../view/member/mypage/Mypage.fxml"));
+			contents.getChildren().removeAll();
+			contents.getChildren().setAll(root);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-   }
-   
-   
-   
-   @FXML
-   public void top50PageChange(ActionEvent event) { //차트메뉴에서 Top50차트 클릭 했을때 페이지 전환 이벤트
-      ChartMenuController.menuCount = 0;
-      chartMenu_PageLoad();
-   }
-   
-   @FXML
-   public void genrePageChange(ActionEvent event) { //차트메뉴에서 장르별차트 클릭 했을때 페이지 전환 이벤트
-      ChartMenuController.menuCount = 1;
-      chartMenu_PageLoad();
-   }
-   
-   @FXML
-   public void periodPageChange(ActionEvent event) { //차트메뉴에서 시대별차트 클릭 했을때 페이지 전환 이벤트
-      ChartMenuController.menuCount = 2;
-      chartMenu_PageLoad();
-   }
-   
-   @FXML
-   public void musicvideoPageChange(ActionEvent event) { //차트메뉴에서 뮤직비디오차트 클릭 했을때 페이지 전환 이벤트
-      ChartMenuController.menuCount = 3;
-      chartMenu_PageLoad();
-   }
-   
-   public void chartMenu_PageLoad() {
-      try {
-         Parent page = FXMLLoader.load(getClass().getResource("../view/chartmenu/main/ChartMenu.fxml")); //바뀔 화면을 가져옴
-         contents.getChildren().removeAll();
-         contents.getChildren().setAll(page);
-      } catch (IOException e) {
-         e.printStackTrace();
-      }
-   }
+	@FXML
+	public void buyTicket() throws IOException {
+		login_PageLoad();
+	}
 
-   public void login_PageLoad() {
-      try {
-         Parent root = FXMLLoader.load(getClass().getResource("../view/login/Login.fxml"));         
-         contents.getChildren().removeAll();
-         contents.getChildren().setAll(root);
-      } catch (IOException e) {
-         e.printStackTrace();
-      }
-   }
-   
-   public void firstPage() {
+	@FXML
+	public void qna() { // 고객센터 - 문의사항
+
+		try {
+			Parent qna = FXMLLoader.load(getClass().getResource("../view/support/qna/QnaMenuList.fxml"));
+			contents.getChildren().removeAll();
+			contents.getChildren().setAll(qna);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@FXML
+	public void top50PageChange(ActionEvent event) { // 차트메뉴에서 Top50차트 클릭 했을때 페이지 전환 이벤트
+		ChartMenuController.menuCount = 0;
+		chartMenu_PageLoad();
+	}
+
+	@FXML
+	public void genrePageChange(ActionEvent event) { // 차트메뉴에서 장르별차트 클릭 했을때 페이지 전환 이벤트
+		ChartMenuController.menuCount = 1;
+		chartMenu_PageLoad();
+	}
+
+	@FXML
+	public void periodPageChange(ActionEvent event) { // 차트메뉴에서 시대별차트 클릭 했을때 페이지 전환 이벤트
+		ChartMenuController.menuCount = 2;
+		chartMenu_PageLoad();
+	}
+
+	@FXML
+	public void musicvideoPageChange(ActionEvent event) { // 차트메뉴에서 뮤직비디오차트 클릭 했을때 페이지 전환 이벤트
+		ChartMenuController.menuCount = 3;
+		chartMenu_PageLoad();
+	}
+
+	public void chartMenu_PageLoad() {
+		try {
+			Parent page = FXMLLoader.load(getClass().getResource("../view/chartmenu/main/ChartMenu.fxml")); // 바뀔 화면을
+																											// 가져옴
+			contents.getChildren().removeAll();
+			contents.getChildren().setAll(page);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void login_PageLoad() {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("../view/login/Login.fxml"));
+			contents.getChildren().removeAll();
+			contents.getChildren().setAll(root);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void firstPage() {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("MusicMain.fxml"));
 		ScrollPane root = null;
 		try {
@@ -199,26 +209,26 @@ public class MusicMainController implements Initializable{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		Scene scene = new Scene(root);
 		Stage primaryStage = (Stage) btn_login.getScene().getWindow();
 		primaryStage.setScene(scene);
-   }
-   
-   @FXML
-	public void singerManage(ActionEvent event) { //가수관리를 클릭 했을 때. 
+	}
+
+	@FXML
+	public void singerManage(ActionEvent event) { // 가수관리를 클릭 했을 때.
 		try {
-			Parent singerManage = FXMLLoader.load(getClass().getResource("../view/singer/singer/ShowSingerList.fxml")); //바뀔 화면을 가져옴
+			Parent singerManage = FXMLLoader.load(getClass().getResource("../view/singer/singer/ShowSingerList.fxml")); // 바뀔
+																														// 화면을
+																														// 가져옴
 			contents.getChildren().removeAll();
 			contents.getChildren().setAll(singerManage);
-			
-		//System.out.println(item_SigerManage.getText());
+
+			// System.out.println(item_SigerManage.getText());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-   
-   
 
 }
