6f429a28ac67c72adc947026d7c8d4df5170ff84
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-07 11:49:35 +0000

    로그인수정/ search 컨트롤러 fx추가
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@53070 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/view/login/LoginController.java
+++ Clap_Client/src/kr/or/ddit/clap/view/login/LoginController.java
@@ -1,7 +1,14 @@
 package kr.or.ddit.clap.view.login;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -10,6 +17,7 @@ import java.rmi.registry.Registry;
 import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -19,9 +27,17 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import kr.or.ddit.clap.main.LoginSession;
 import kr.or.ddit.clap.main.MusicMainController;
@@ -43,10 +59,23 @@ public class LoginController implements Initializable{
 	LoginSession ls = new LoginSession();
 	
 	@FXML JFXTextField txt_id;
+	@FXML JFXTextField txt_captcha;
 	@FXML JFXPasswordField txt_pw;
+	@FXML Button btn_login;
+	@FXML HBox box1;
+	@FXML HBox box2;
+	@FXML HBox box3;
 	@FXML Label lb_check;
+	@FXML Label lb_captcha;
+	@FXML Label lb_ok;
+	@FXML ImageView img_captcha;
 	
 	List<MemberVO> list = new ArrayList<MemberVO>();
+	
+	private String captchaKey = "";
+	File f = null; // captchaimg파일
+	private boolean captchaFlag = false;
+	private int errorCount = 0; // id/pw 불일치 횟수.
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -58,6 +87,9 @@ public class LoginController implements Initializable{
 		} catch (NotBoundException e) {
 			e.printStackTrace();
 		}
+		
+		captchaKey = captchaKey();
+		captchaImage(captchaKey);
 	}
 	
 	public void login() throws UnsupportedEncodingException, NoSuchAlgorithmException, GeneralSecurityException{
@@ -70,7 +102,25 @@ public class LoginController implements Initializable{
 		decryptedPw = aes.decrypt(encryptedPw);
 		
 		MemberVO vo = new MemberVO();
+		
+		box1.setVisible(false);
+		box2.setVisible(false);
+		lb_captcha.setVisible(false);
+		txt_captcha.setText("");
+		if(errorCount==1) {
+			imgRefresh();
+		}
 	
+		if(txt_id.getText().equals("")) {
+			lb_check.setVisible(true);
+			lb_check.setText("아이디를 입력하세요.");
+			return;
+		}else if(txt_pw.getText().equals("")) {
+			lb_check.setVisible(true);
+			lb_check.setText("비밀번호를 입력하세요.");
+			return;
+		}
+		
 		// 아이디 확인
 		Boolean idCheck = false;
 		try {
@@ -81,8 +131,10 @@ public class LoginController implements Initializable{
 		}
 		
 		System.out.println("아이디체크 " + idCheck);
+
 		if(!idCheck) {
 			lb_check.setVisible(true);
+			lb_check.setText("존재하지 않는 아이디입니다.");
 			return;
 		}
 		
@@ -116,21 +168,181 @@ public class LoginController implements Initializable{
 			primaryStage.setScene(scene);
 		}else {
 			System.out.println("비밀번호 불일치");
+			if(!txt_id.getText().equals("") && !txt_pw.getText().equals("")) {
+				errorCount++;				
+			}
+			if(errorCount==3) {
+				lb_check.setVisible(true);
+				lb_check.setText("로그인 정보 3회 오류. 보안문자를 입력해주세요.");
+				btn_login.setDisable(true);
+				box1.setVisible(true);
+				box2.setVisible(true);
+				return;
+			}
 			lb_check.setVisible(true);
+			lb_check.setText("아이디 또는 비밀번호를 확인해주세요. ("+errorCount+"/3회 오류)");
 		}
-		
-		// 비밀번호가 일치하지 않을 때
-//		if(false) {
-//			System.out.println("비밀번호를 정확히 입력해주세요.");
-//			// 3회 이상 실패하면 captcha.
-//		}
-		
-		
-		
-		
-		
 		
 		
 	}
 
+	public void imgRefresh() {
+		captchaKey = captchaKey();
+		captchaImage(captchaKey);
+	}
+	
+	public String captchaKey() {
+        String clientId = "klxHW7Dv5xl3eCyGm4My";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "FLAfQWcfxj";//애플리케이션 클라이언트 시크릿값";
+        String result = "";
+        try {
+            String code = "0"; // 키 발급시 0,  캡차 이미지 비교시 1로 세팅
+            String apiURL = "https://openapi.naver.com/v1/captcha/nkey?code=" + code;
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("X-Naver-Client-Id", clientId);
+            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 에러 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            System.out.println(response.toString());
+            result = response.toString().split("\"")[3];
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+		return result;
+	}
+	
+	public void captchaImage(String CaptchaKey) {
+        String clientId = "klxHW7Dv5xl3eCyGm4My";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "FLAfQWcfxj";//애플리케이션 클라이언트 시크릿값";
+        try {
+            String key = CaptchaKey; // https://openapi.naver.com/v1/captcha/nkey 호출로 받은 키값
+            String apiURL = "https://openapi.naver.com/v1/captcha/ncaptcha.bin?key=" + key;
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("X-Naver-Client-Id", clientId);
+            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                InputStream is = con.getInputStream();
+                int read = 0;
+                byte[] bytes = new byte[1024];
+                f = new File("captchaImg.jpg");
+                f.createNewFile();
+                OutputStream outputStream = new FileOutputStream(f);
+                while ((read =is.read(bytes)) != -1) {
+                    outputStream.write(bytes, 0, read);
+                }
+                is.close();
+            } else {  // 에러 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = br.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                br.close();
+                System.out.println(response.toString());
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+		Image captcha = new Image("file:"+f.getAbsolutePath());
+		img_captcha.setImage(captcha);
+	}
+	
+	public void captchaCheck() {
+		String result = captchaResult(captchaKey, txt_captcha.getText());
+		if(txt_captcha.getText().equals("")) {
+			lb_captcha.setVisible(true);
+			lb_captcha.setText("보안문자를 입력해주세요.");
+			captchaFlag = false;
+			lb_captcha.setTextFill(Color.RED);
+			txt_captcha.requestFocus();	
+		}else if(result.equals("false")) {
+			imgRefresh();
+			lb_captcha.setVisible(true);
+			lb_captcha.setPadding(new Insets(0));
+			lb_captcha.setText("보안문자가 일치하지 않습니다. 새로 입력해주세요.");
+			captchaFlag = false;
+			lb_captcha.setTextFill(Color.RED);
+			txt_captcha.requestFocus();	
+		}else if(result.equals("true,")) {
+			lb_captcha.setVisible(true);
+			lb_captcha.setPadding(new Insets(3));
+			lb_captcha.setText("확인되었습니다.");
+			btn_login.setDisable(false);
+			errorCount = 0;
+			lb_check.setVisible(false);
+			captchaFlag = true;
+			lb_captcha.setTextFill(Color.valueOf("#00cc00"));
+		}
+	}
+	
+	public String captchaResult(String CaptchaKey, String input) {
+        String clientId = "klxHW7Dv5xl3eCyGm4My";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "FLAfQWcfxj";//애플리케이션 클라이언트 시크릿값";
+        String result = "";
+        try {
+            String code = "1"; // 키 발급시 0,  캡차 이미지 비교시 1로 세팅
+            String key = CaptchaKey; // 캡차 키 발급시 받은 키값
+            String value = input; // 사용자가 입력한 캡차 이미지 글자값
+            String apiURL = "https://openapi.naver.com/v1/captcha/nkey?code=" + code +"&key="+ key + "&value="+ value;
+
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("X-Naver-Client-Id", clientId);
+            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 에러 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            System.out.println(response.toString());
+            result = response.toString().substring(10, 15);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+		return result;
+	}
+	
+	public void search() {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("Search.fxml"));
+		BorderPane root = null;
+		try {
+			root = loader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Scene scene = new Scene(root);
+		Stage primaryStage = (Stage) txt_id.getScene().getWindow();
+		primaryStage.setScene(scene);
+	}
+	
 }
