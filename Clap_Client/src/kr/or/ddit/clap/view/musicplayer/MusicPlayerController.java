dd60c52382372d59cca35ac98aeeb826550b8b39
201809_JMK <201809_JMK@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-07 12:50:21 +0000

    musicplayercontroller class 추가
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@53115 d808369f-3623-194b-b0f8-88541be8a525

---
+++ Clap_Client/src/kr/or/ddit/clap/view/musicplayer/MusicPlayerController.java
@@ -0,0 +1,15 @@
+package kr.or.ddit.clap.view.musicplayer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class MusicPlayerController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+
+}
