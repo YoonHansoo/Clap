09e3e929364953beb480715f710c38f11eda3beb
201809_JMK <201809_JMK@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-08 05:45:09 +0000

    mp3파일 저장 (Test)
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@53192 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/view/musicplayer/MusicPlayerController.java
+++ Clap_Client/src/kr/or/ddit/clap/view/musicplayer/MusicPlayerController.java
@@ -1,15 +1,30 @@
 package kr.or.ddit.clap.view.musicplayer;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.fxml.FXML;
+import com.jfoenix.controls.JFXButton;
 
 public class MusicPlayerController implements Initializable{
+	private MediaPlayer mediaPlayer;
+	private Media media;
+	private String path;
+	@FXML JFXButton play; 
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+		path = "kr/or/ddit/clap/view/musicplayer/1.mp3";
+		media = new Media(new File(path).toURI().toString());
+		mediaPlayer = new MediaPlayer(media);
+	}
+
+	@FXML public void onPlay() {
+		mediaPlayer.play();
 	}
 
 }
