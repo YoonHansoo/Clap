94ea5afed0e5e7fbde285ec18f7cf13cd01324e1
201809_KHJ <201809_KHJ@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-08 05:37:19 +0000

    프로필수정 수정중
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@53188 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/view/member/mypage/MypageController.java
+++ Clap_Client/src/kr/or/ddit/clap/view/member/mypage/MypageController.java
@@ -1,5 +1,7 @@
 package kr.or.ddit.clap.view.member.mypage;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.rmi.NotBoundException;
@@ -8,7 +10,10 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.ResourceBundle;
 
+import javax.imageio.ImageIO;
+
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXTreeTableView;
@@ -18,20 +23,25 @@ import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import kr.or.ddit.clap.main.LoginSession;
@@ -40,53 +50,78 @@ import kr.or.ddit.clap.service.musicreview.IMusicReviewService;
 import kr.or.ddit.clap.service.mypage.IMypageService;
 import kr.or.ddit.clap.vo.member.MemberVO;
 import kr.or.ddit.clap.vo.music.MusicHistoryVO;
-import kr.or.ddit.clap.vo.music.MusicReviewVO;
-import javafx.scene.image.ImageView;;
+import kr.or.ddit.clap.vo.music.MusicReviewVO;;
 
 public class MypageController implements Initializable {
-	int no1=0;
-	int no2=0;
-	int no3=0;
+	int no1 = 0;
+	int no2 = 0;
+	int no3 = 0;
 	static Stage mypageDialog = new Stage(StageStyle.DECORATED);
 	static Stage pwok = new Stage(StageStyle.DECORATED);
-	
+
+	public Stage primaryStage;
+
+	private FileChooser fileChooser;
+	private String img_path;
+	private File filePath;
 	private Registry reg;
 	private IMypageService ims;
 	private IMusicReviewService imrs;
 	private IMusicHistoryService imhs;
 	private ObservableList<MusicReviewVO> revList, currentrevList;
-	private ObservableList<MusicHistoryVO> singList;	//최근음악 담기
+	private ObservableList<MusicHistoryVO> singList; // 최근음악 담기
 	private ObservableList<MusicHistoryVO> newList;
-	
-	private String temp_img_path="";
-	
-	@FXML Label label_Id;
-	@FXML Image img_User;
-	@FXML AnchorPane contents;
-	@FXML Text text_UserInfo;
-	@FXML AnchorPane InfoContents;
-	@FXML AnchorPane Head;
-	
-	@FXML JFXTreeTableView<MusicReviewVO> tbl_Review; 	//리뷰
-	@FXML TreeTableColumn<MusicReviewVO, String> col_ReviewCont;
-	@FXML TreeTableColumn<MusicReviewVO, String> col_ReviewDate;
 
-	@FXML JFXTreeTableView<MusicHistoryVO> tbl_ManySigner;	//많이들은 아티스트
-	@FXML TreeTableColumn col_MSno;
-	@FXML TreeTableColumn<MusicHistoryVO, String> col_MSits;
-	
-	@FXML JFXTreeTableView<MusicHistoryVO> tbl_ManyMusic;	//많이들은 곡
-	@FXML TreeTableColumn col_MMno;
-	@FXML TreeTableColumn<MusicHistoryVO, String> col_MMits;
-	@FXML TreeTableColumn<MusicHistoryVO, String> col_MMtitle;
-	
-	@FXML JFXTreeTableView tbl_NewMusic;					//최근들은곡
-	@FXML TreeTableColumn col_NMno;
-	@FXML TreeTableColumn<MusicHistoryVO, String> col_NMits;
-	@FXML TreeTableColumn<MusicHistoryVO, String> col_NMtitle;
-	@FXML TreeTableColumn<MusicHistoryVO, String> col_NMdate;
-	@FXML ImageView img_UserImg;
+	private String temp_img_path = "";
 
+	@FXML
+	Label label_Id;
+	@FXML
+	Image img_User;
+	@FXML
+	AnchorPane contents;
+	@FXML
+	Text text_UserInfo;
+	@FXML
+	AnchorPane InfoContents;
+	@FXML
+	AnchorPane Head;
+
+	@FXML
+	JFXTreeTableView<MusicReviewVO> tbl_Review; // 리뷰
+	@FXML
+	TreeTableColumn<MusicReviewVO, String> col_ReviewCont;
+	@FXML
+	TreeTableColumn<MusicReviewVO, String> col_ReviewDate;
+
+	@FXML
+	JFXTreeTableView<MusicHistoryVO> tbl_ManySigner; // 많이들은 아티스트
+	@FXML
+	TreeTableColumn col_MSno;
+	@FXML
+	TreeTableColumn<MusicHistoryVO, String> col_MSits;
+
+	@FXML
+	JFXTreeTableView<MusicHistoryVO> tbl_ManyMusic; // 많이들은 곡
+	@FXML
+	TreeTableColumn col_MMno;
+	@FXML
+	TreeTableColumn<MusicHistoryVO, String> col_MMits;
+	@FXML
+	TreeTableColumn<MusicHistoryVO, String> col_MMtitle;
+
+	@FXML
+	JFXTreeTableView tbl_NewMusic; // 최근들은곡
+	@FXML
+	TreeTableColumn col_NMno;
+	@FXML
+	TreeTableColumn<MusicHistoryVO, String> col_NMits;
+	@FXML
+	TreeTableColumn<MusicHistoryVO, String> col_NMtitle;
+	@FXML
+	TreeTableColumn<MusicHistoryVO, String> col_NMdate;
+	@FXML
+	ImageView img_UserImg;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -106,7 +141,7 @@ public class MypageController implements Initializable {
 		label_Id.setText(user_id); // 현재 로그인한 사용자 아이디 가져오기
 		MemberVO vo = new MemberVO();
 		vo.setMem_id(user_id);
-		MemberVO memvo =new MemberVO();
+		MemberVO memvo = new MemberVO();
 		try {
 			memvo = ims.select(vo);
 			text_UserInfo.setText(memvo.getMem_intro());
@@ -114,39 +149,37 @@ public class MypageController implements Initializable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		//이미지 설정
+
+		// 이미지 설정
 		Image img = new Image(memvo.getMem_image());
-		temp_img_path = memvo.getMem_image();   //sVO.getSing_image()를 전역으로 쓰기위해
+		temp_img_path = memvo.getMem_image(); // sVO.getSing_image()를 전역으로 쓰기위해
 		img_UserImg.setImage(img);
-		
-		
-		//최근댓글테이블
-		col_ReviewCont.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getValue().getMus_re_content()));
+
+		// 최근댓글테이블
+		col_ReviewCont.setCellValueFactory(
+				param -> new SimpleStringProperty(param.getValue().getValue().getMus_re_content()));
 		col_ReviewDate.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getValue().getIndate()));
-	
-		//최근많이 들은 아티스트이름넣기 
-		col_MSno.setCellValueFactory(param -> new SimpleStringProperty(""+(no1++)));
+
+		// 최근많이 들은 아티스트이름넣기
+		col_MSno.setCellValueFactory(param -> new SimpleStringProperty("" + (no1++)));
 		col_MSits.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getValue().getName()));
 
-		//최근많이 들은 곡
-		col_MMno.setCellValueFactory(param -> new SimpleStringProperty(""+(no2++)));
+		// 최근많이 들은 곡
+		col_MMno.setCellValueFactory(param -> new SimpleStringProperty("" + (no2++)));
 		col_MMits.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getValue().getName()));
 		col_MMtitle.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getValue().getTitle()));
-	
-		//최근 감상곡
-		col_NMno.setCellValueFactory(param -> new SimpleStringProperty(""+(no3++)));
+
+		// 최근 감상곡
+		col_NMno.setCellValueFactory(param -> new SimpleStringProperty("" + (no3++)));
 		col_NMits.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getValue().getName()));
 		col_NMtitle.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getValue().getTitle()));
-		col_NMdate.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getValue().getHisto_indate().substring(0,10)));
-		
-		
-		
-		
+		col_NMdate.setCellValueFactory(
+				param -> new SimpleStringProperty(param.getValue().getValue().getHisto_indate().substring(0, 10)));
+
 		// 데이터 삽입
-		MusicReviewVO muvo =new MusicReviewVO();
+		MusicReviewVO muvo = new MusicReviewVO();
 		muvo.setMem_id(user_id);
-		MusicHistoryVO muh =new MusicHistoryVO();
+		MusicHistoryVO muh = new MusicHistoryVO();
 		muh.setMem_id(user_id);
 		try {
 			revList = FXCollections.observableArrayList(imrs.selectReview(muvo));
@@ -155,34 +188,125 @@ public class MypageController implements Initializable {
 		} catch (RemoteException e) {
 			System.out.println("에러");
 			e.printStackTrace();
-		}	
-		
+		}
+
 		TreeItem<MusicReviewVO> root = new RecursiveTreeItem<>(revList, RecursiveTreeObject::getChildren);
 		tbl_Review.setRoot(root);
 		tbl_Review.setShowRoot(false);
-		
+
 		TreeItem<MusicHistoryVO> root1 = new RecursiveTreeItem<>(singList, RecursiveTreeObject::getChildren);
 		tbl_ManySigner.setRoot(root1);
 		tbl_ManySigner.setShowRoot(false);
 		tbl_ManyMusic.setRoot(root1);
 		tbl_ManyMusic.setShowRoot(false);
-		
+
 		TreeItem<MusicHistoryVO> root2 = new RecursiveTreeItem<>(newList, RecursiveTreeObject::getChildren);
 		tbl_NewMusic.setRoot(root2);
 		tbl_NewMusic.setShowRoot(false);
-		
-		
 
 	}
 
 	@FXML
-	public void btn_profch() throws IOException { // 프로필 수정 클릭시
-		Parent root = FXMLLoader.load(getClass().getResource("../profilech/profile.fxml"));
+	public void btn_profch() throws IOException {// 프로필 수정 클릭시
+		Parent root = FXMLLoader.load(getClass().getResource("profile.fxml"));
 		Scene scene = new Scene(root);
 		mypageDialog.setTitle("모여서 각잡고 코딩 - clap");
 
 		mypageDialog.setScene(scene);
 		mypageDialog.show();
+
+	/*	Button btnCl = (Button) root.lookup("#btn_ProfileCl");
+		btnCl.setOnAction(e -> {
+			mypageDialog.close();
+		});
+
+		String user_id = LoginSession.session.getMem_id();
+		MemberVO vo = new MemberVO();
+		vo.setMem_id(user_id);
+		MemberVO memvo = new MemberVO();
+		try {
+			memvo = ims.select(vo);
+			TextField textF_Info = (TextField) root.lookup("#textF_Info");
+			textF_Info.setText(memvo.getMem_intro());
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		Image img = new Image(memvo.getMem_image());
+		img_path = memvo.getMem_image(); // sVO.getSing_image()를 전역으로 쓰기위해
+		ImageView imgview_UserImg = (ImageView) root.lookup("#imgview_UserImg");
+		imgview_UserImg.setImage(img);
+
+		Button btn_Image = (Button) root.lookup("#btn_Image");
+
+		btn_Image.setOnAction(e1 -> {
+			Stage stage = (Stage) ((Node) e1.getSource()).getScene().getWindow();
+			fileChooser = new FileChooser();
+			fileChooser.setTitle("Open image");
+
+			// 사용자의 디렉토리 보여줌
+			// String userDirectoryString = System.getProperty("user.home") + "\\Pictures";
+			// 기본위치
+			String userDirectoryString = "\\\\Sem-pc\\공유폴더\\Clap\\img\\singer";
+
+			System.out.println("userDirectoryString:" + userDirectoryString);
+			File userDirectory = new File(userDirectoryString);
+
+			if (!userDirectory.canRead()) {
+				userDirectory = new File("c:/");
+			}
+
+			fileChooser.setInitialDirectory(userDirectory);
+			;
+
+			this.filePath = fileChooser.showOpenDialog(stage);
+
+			// 이미지를 새로운 이미지로 바꿈
+			try {
+				BufferedImage bufferedImage = ImageIO.read(filePath);
+				Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+				imgview_UserImg.setImage(image);
+				System.out.println("파일경로:" + filePath);
+				String str_filePath = "file:" + filePath;
+				// userDirectoryString = "file:\\\\Sem-pc\\공유폴더\\Clap\\img\\singer"; //화면 출력 시
+				// 절대경로로 이미지를 읽기위해서
+				img_path = str_filePath;
+
+			} catch (Exception e) {
+				// System.out.println(e.getMessage());
+				// e.printStackTrace();
+				System.out.println("이미지를 선택하지 않았습니다.");
+			}
+		});
+
+		JFXCheckBox chBox_del = (JFXCheckBox) root.lookup("#chBox_del");
+		chBox_del.setOnAction(ee -> {
+			TextField textF_Info = (TextField) root.lookup("#textF_Info");
+			textF_Info.setText("");
+		});// x표시 클릭시
+
+		Button btn_ProfileOk = (Button) root.lookup("#btn_ProfileOk");// 설정 클릭시
+		btn_ProfileOk.setOnAction(eee -> {
+			
+			TextField textF_Info = (TextField) root.lookup("#textF_Info");
+			System.out.println(textF_Info.getText());
+			System.out.println(img_path);
+			MemberVO vo1 = new MemberVO();
+			vo.setMem_intro(textF_Info.getText());
+			vo.setMem_image(img_path);
+			vo.setMem_id(user_id);
+			try {
+				int c=ims.updateImage(vo1);
+
+				System.out.println(c);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+
+			}
+			mypageDialog.close();
+		});*/
+
 	}
 
 	@FXML
@@ -243,47 +367,44 @@ public class MypageController implements Initializable {
 		});
 
 	}
+
 	@FXML
-	public void btn_Info() throws IOException {  //열필모양 (사용자 개인소개)수정
-		
-		
-		
-		Parent root=null;
+	public void btn_Info() throws IOException { // 열필모양 (사용자 개인소개)수정
+
+		Parent root = null;
 		try {
-			 root = FXMLLoader.load(getClass().getResource("mypageSub.fxml"));
+			root = FXMLLoader.load(getClass().getResource("mypageSub.fxml"));
 			InfoContents.getChildren().removeAll();
 			InfoContents.getChildren().setAll(root);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 
 		String user_id = LoginSession.session.getMem_id();
 		MemberVO vo = new MemberVO();
 		vo.setMem_id(user_id);
-		MemberVO memvo =new MemberVO();
+		MemberVO memvo = new MemberVO();
 		try {
 			memvo = ims.select(vo);
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
-		
-		JFXButton btn_SubOk =(JFXButton)root.lookup("#btn_SubOk");
-		JFXButton btn_SubCl =(JFXButton)root.lookup("#btn_SubCl");
-		JFXTextField textF_Sub=  (JFXTextField)root.lookup("#textF_Sub");
+
+		JFXButton btn_SubOk = (JFXButton) root.lookup("#btn_SubOk");
+		JFXButton btn_SubCl = (JFXButton) root.lookup("#btn_SubCl");
+		JFXTextField textF_Sub = (JFXTextField) root.lookup("#textF_Sub");
 		textF_Sub.setText(memvo.getMem_intro());
-		
-		
-		btn_SubOk.setOnAction(e1->{
+
+		btn_SubOk.setOnAction(e1 -> {
 			vo.setMem_intro(textF_Sub.getText());
 			try {
-				 ims.updateInfo(vo);
+				ims.updateInfo(vo);
 			} catch (RemoteException e) {
 				e.printStackTrace();
 			}
-			
+
 			System.out.println("완료");
-			
+
 			try {
 				Parent root2 = FXMLLoader.load(getClass().getResource("Mypage.fxml"));
 				Head.getChildren().removeAll();
@@ -294,34 +415,32 @@ public class MypageController implements Initializable {
 				e.printStackTrace();
 			}
 		});
-		
-		btn_SubCl.setOnAction(e1->{try {
-			Parent root2 = FXMLLoader.load(getClass().getResource("Mypage.fxml"));
-			Head.getChildren().removeAll();
-			InfoContents.getChildren().removeAll();
-			contents.getChildren().removeAll();
-			Head.getChildren().setAll(root2);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}});
+
+		btn_SubCl.setOnAction(e1 -> {
+			try {
+				Parent root2 = FXMLLoader.load(getClass().getResource("Mypage.fxml"));
+				Head.getChildren().removeAll();
+				InfoContents.getChildren().removeAll();
+				contents.getChildren().removeAll();
+				Head.getChildren().setAll(root2);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
 	}
-	
-	
-	
+
 	@FXML
-	public void btn_like() throws IOException {   //좋아요클릭시
-		
+	public void btn_like() throws IOException { // 좋아요클릭시
+
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("../profiles/like.fxml"));
 			contents.getChildren().removeAll();
 			contents.getChildren().setAll(root);
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
 
 }
