3ccb70306a17107c27604fa4e3b119fbc7c6f62d
201809_JMK <201809_JMK@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-08 13:58:54 +0000

    뮤직플레이어 재생/일시정지/시간표시/슬라이더로 되감기 빨리감기 구현
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@53353 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/view/musicplayer/MusicPlayer.java
+++ Clap_Client/src/kr/or/ddit/clap/view/musicplayer/MusicPlayer.java
@@ -1,5 +1,83 @@
 package kr.or.ddit.clap.view.musicplayer;
 
-public class MusicPlayer {
+import java.io.File;
+import com.jfoenix.controls.JFXSlider;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaPlayer.Status;
+import javafx.util.Duration;
 
+    public class   MusicPlayer {
+	private MediaPlayer mediaPlayer;
+	private String path;
+	private Media media;
+	private Status status;
+	
+	
+	public MusicPlayer() {
+		
+	}
+	
+	public MusicPlayer(String path) {
+		media = new Media(new File(path).toURI().toString());
+		mediaPlayer = new MediaPlayer(media);
+	}
+	
+	public void Ready(Label Label_nowTime, Label Label_finalTime, JFXSlider slider_time) {
+		
+		mediaPlayer.setOnReady(() -> {
+		
+			int totalSeconds = (int) Math.floor(mediaPlayer.getTotalDuration().toSeconds());
+			int tMinutes = totalSeconds / 60;
+			int tSeconds = totalSeconds - tMinutes * 60;
+			
+			Label_finalTime.setText(String.format("%02d:%02d",tMinutes,tSeconds));
+				
+			mediaPlayer.currentTimeProperty().addListener((observable, oldValue, newValue) -> {
+					
+				int nowSeconds = (int) Math.floor(mediaPlayer.getCurrentTime().toSeconds());
+				int minutes = nowSeconds / 60;
+				int seconds = nowSeconds - minutes * 60;
+				
+				Label_nowTime.setText(String.format("%02d:%02d",minutes,seconds));
+				double nowTime = mediaPlayer.getCurrentTime().toSeconds() / 
+				                 mediaPlayer.getTotalDuration().toSeconds() * 100;
+				
+				if (!slider_time.isValueChanging()) {
+					slider_time.setValue(nowTime);
+				}
+			});
+		});
+	}
+	
+	public void play(FontAwesomeIcon icon_play) {
+		status = mediaPlayer.getStatus();
+		
+		if (status == Status.PAUSED ||
+			status == Status.READY ||
+			status == Status.STOPPED) {
+			icon_play.setIconName("PAUSE");
+			mediaPlayer.play();
+		}else  {
+			icon_play.setIconName("PLAY");
+			mediaPlayer.pause();
+		}
+	}
+	
+	public void stop() {
+		mediaPlayer.stop();
+	}
+	
+	public Status getStatus() {
+		return mediaPlayer.getStatus();
+	}
+	
+	public void musicSync(double value) {
+		double time = (mediaPlayer.getTotalDuration().toSeconds() * 1000 / 100) * value;
+		mediaPlayer.seek(new Duration(time));
+	}
 }
--- Clap_Client/src/kr/or/ddit/clap/view/musicplayer/MusicPlayerController.java
+++ Clap_Client/src/kr/or/ddit/clap/view/musicplayer/MusicPlayerController.java
@@ -4,12 +4,12 @@ import java.io.File;
 import java.net.URL;
 import java.text.Format;
 import java.util.ResourceBundle;
-
 import javafx.fxml.Initializable;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaPlayer.Status;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import kr.or.ddit.clap.main.MusicMainController;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
@@ -22,71 +22,41 @@ import com.jfoenix.controls.JFXSlider;
 import javafx.scene.control.Label;
 
 public class MusicPlayerController implements Initializable{
-	private Stage stage;
-	private MediaPlayer mediaPlayer;
-	private Media media;
-	private String path;
-	private Status status;
 	
 	@FXML FontAwesomeIcon icon_play;
-	@FXML JFXSlider slider_time;
+	@FXML 
+	 JFXSlider slider_time;
 	@FXML Label Label_nowTime;
 	@FXML Label Label_finalTime; 
+	
+	private Stage stage;
+	private String path;
+	private MusicPlayer player;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		stage = MusicMainController.musicplayer;
-		path = "////Sem-pc//공유폴더//Clap//mp3//1.mp3";
-		media = new Media(new File(path).toURI().toString());
-		mediaPlayer = new MediaPlayer(media);
+//		path = "C://Users//jin//Downloads//3.mp3";
+		path = "////Sem-pc//공유폴더//Clap//mp3//2.mp3";
+		player = new MusicPlayer(path);
 		
-		stage.setOnCloseRequest(e -> {
-			mediaPlayer.stop();
+
+			
+		player.Ready(Label_nowTime, Label_finalTime, slider_time);
+		
+		
+		stage.setOnCloseRequest(e -> { 
+			player.stop();
 		});
 		
-		mediaPlayer.setOnReady(new Runnable() {
-			
-			
-			@Override
-			public void run() {
-				int totalSeconds = (int) Math.floor(mediaPlayer.getTotalDuration().toSeconds());
-				int tMinutes = totalSeconds / 60;
-				int tSeconds = totalSeconds - tMinutes * 60;
-				
-				Label_finalTime.setText(String.format("%02d:%02d",tMinutes,tSeconds));
-				
-				mediaPlayer.currentTimeProperty().addListener(new ChangeListener() {
-					@Override
-					public void changed(ObservableValue observable, Object oldValue, Object newValue) {
-					
-					int nowSeconds = (int) Math.floor(mediaPlayer.getCurrentTime().toSeconds());
-					int minutes = nowSeconds / 60;
-					int seconds = nowSeconds - minutes * 60;
-					
-					Label_nowTime.setText(String.format("%02d:%02d",minutes,seconds));
-					double nowTime = mediaPlayer.getCurrentTime().toSeconds() / 
-					                 mediaPlayer.getTotalDuration().toSeconds() * 100;
-						slider_time.setValue(nowTime);
-					}
-				});
-			}
+		slider_time.setOnMouseClicked(e->{
+			player.musicSync(slider_time.getValue());
 		});
-		
 		
 	}
 
 	@FXML public void onPlay() {
-		status = mediaPlayer.getStatus();
-		
-		if (status == Status.PAUSED ||
-			status == Status.READY ||
-			status == Status.STOPPED) {
-			icon_play.setIconName("PAUSE");
-			mediaPlayer.play();
-		}else  {
-			icon_play.setIconName("PLAY");
-			mediaPlayer.pause();
-		}
+		player.play(icon_play);
 	}
 
 }
