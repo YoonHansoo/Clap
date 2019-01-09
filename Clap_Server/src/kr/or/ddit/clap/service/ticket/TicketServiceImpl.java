c7516c4bdbd8b8cc1e50118d0e22d617f71d00a4
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-09 12:47:17 +0000

    이용권 쿼리수정
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@53561 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Server/src/kr/or/ddit/clap/service/ticket/TicketServiceImpl.java
+++ Clap_Server/src/kr/or/ddit/clap/service/ticket/TicketServiceImpl.java
@@ -1,12 +1,14 @@
 package kr.or.ddit.clap.service.ticket;
 
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.List;
 
 import kr.or.ddit.clap.dao.ticket.TicketDaoImpl;
 import kr.or.ddit.clap.vo.ticket.TicketBuyListVO;
 
-public class TicketServiceImpl implements ITicketService{
+public class TicketServiceImpl extends UnicastRemoteObject implements ITicketService{
 
 	TicketDaoImpl ticketDao; // 사용할 Dao의  멤버변수를 선언
 	private static TicketServiceImpl service; // Singleton패턴
@@ -26,6 +28,23 @@ public class TicketServiceImpl implements ITicketService{
 	@Override
 	public List<TicketBuyListVO> selectList(String id) throws RemoteException {
 		return ticketDao.selectList(id);
+	}
+	
+	public static void main(String[] args) {
+		try {
+			service = service.getInstance();
+			List<TicketBuyListVO> list = new ArrayList<TicketBuyListVO>();
+			list = service.selectList("admin1");
+			System.out.println("size  "+list.size());
+			System.out.println(list.get(0).getTicket_no());
+			System.out.println(list.get(1).getTicket_no());
+			System.out.println(list.get(2).getTicket_no());
+			System.out.println(list.get(3).getTicket_no());
+			System.out.println(list.get(4).getTicket_no());
+			
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
