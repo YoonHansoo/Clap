20bc07c2b9a70146c825b926caa8dd6510a9a854
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-09 05:15:23 +0000

    임시로그인 수정
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@53407 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/main/ClientMain.java
+++ Clap_Client/src/kr/or/ddit/clap/main/ClientMain.java
@@ -1,10 +1,18 @@
 package kr.or.ddit.clap.main;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
+import kr.or.ddit.clap.service.login.ILoginService;
 import kr.or.ddit.clap.vo.member.MemberVO;
 
 /**
@@ -16,26 +24,37 @@ import kr.or.ddit.clap.vo.member.MemberVO;
  */
 
 public class ClientMain extends Application {
+	
+	private ILoginService ils;
+	private Registry reg;
+	List<MemberVO> list = new ArrayList<MemberVO>();
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		LoginSession ls = new LoginSession();
 
-		LoginSession.session = new MemberVO(); 
-		LoginSession.session.setMem_id("user1");
-		LoginSession.session.setMem_pw("1234");
-		LoginSession.session.setMem_gender("m");
-		LoginSession.session.setMem_name("윤한수");
-		LoginSession.session.setMem_auth("t");
-
-			System.out.println("Start Clap!");
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("MusicMain.fxml"));
-			ScrollPane root = loader.load();
+		// 임시로그인 부분
+		try {
+			reg = LocateRegistry.getRegistry("localhost", 8888);
+			ils = (ILoginService) reg.lookup("login");
 			
-			Scene scene = new Scene(root);
-			primaryStage.setTitle("Clap:음악, 그리고 설레임");
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			list = ils.select("admin1");
+			ls.session = list.get(0);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
+		// 임시로그인 부분
+
+		System.out.println("Start Clap!");
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("MusicMain.fxml"));
+		ScrollPane root = loader.load();
+		
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Clap:음악, 그리고 설레임");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 		
 	}
 	public static void main(String[] args) {
