ba95087d675ed5298f955cbe8d1cc5f4fa9543d6
201809_LHH <201809_LHH@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-10 09:46:59 +0000

    event
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@53760 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/view/support/eventboard/EventContentInsertController.java
+++ Clap_Client/src/kr/or/ddit/clap/view/support/eventboard/EventContentInsertController.java
@@ -1,11 +1,16 @@
 /**
- * 이벤트관리 emdfhr화면 컨트롤러
+ * 이벤트관리 등록 화면 컨트롤러
  * @author Hanhwa
  * 
  */
 package kr.or.ddit.clap.view.support.eventboard;
 
+import java.io.File;
 import java.net.URL;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
@@ -13,10 +18,17 @@ import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 
 import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import kr.or.ddit.clap.service.eventboard.IEventBoardService;
+import kr.or.ddit.clap.service.singer.ISingerService;
 
 
 
@@ -45,17 +57,51 @@ public class EventContentInsertController implements Initializable {
 	JFXTextArea text_Content;
 	
 	public static AnchorPane contents;
+	private FileChooser fileChooser;
+	private File filePath;
+	private String img_path;
+	private Registry reg;
+	private IEventBoardService ies;
 	
 	//ShowSingerList.fxml는 VBOX를 포함한 전부이기 때문에
 	//현재 씬의 VBox까지 모두 제거 후   ShowSingerList를 불러야함.
 	public void givePane(AnchorPane contents) {
 		this.contents = contents;
 		System.out.println("contents 적용완료");
+		
+	}
+	
+	// 전 화면에 있는 데이터를 그대로 가져와  세팅해주는 메서드
+	public void initData() {
+		System.out.println("initData");
+		
+		img_path = "file:\\\\\\\\Sem-pc\\\\공유폴더\\\\Clap\\\\img\\\\noImg.png";
+		Image img = new Image(img_path); 
+		imgview_singImg.setImage(img);
 	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		
+		try {
+			reg = LocateRegistry.getRegistry("localhost", 8888);
+			ies = (IEventBoardService) reg.lookup("eventboard");
+			initData();
+		} catch(RemoteException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
+		
+		/*@FXML
+		public void btn_insertImg(ActionEvent event) {
+			Stage stage =  (Stage) ((Node)event.getSource()).getScene().getWindow();
+			 fileChooser = new FileChooser();
+			 fileChooser.setTitle("Open image");
+		}*/
+		
+		
+		
 		
 	}
 
