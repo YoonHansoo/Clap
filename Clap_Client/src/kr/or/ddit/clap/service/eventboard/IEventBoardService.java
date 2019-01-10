0320b06457d389392f52a1c191e2428cb42f07c4
201809_LHH <201809_LHH@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-10 09:05:07 +0000

    client - service - IEventBoardService
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@53753 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/service/eventboard/IEventBoardService.java
+++ Clap_Client/src/kr/or/ddit/clap/service/eventboard/IEventBoardService.java
@@ -1,5 +1,18 @@
 package kr.or.ddit.clap.service.eventboard;
 
-public interface IEventBoardService {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+import kr.or.ddit.clap.vo.support.EventBoardVO;
+
+public interface IEventBoardService extends Remote {
+	
+	public List<EventBoardVO> selectListAll() throws RemoteException;
+	
+	public List<EventBoardVO> searchList(EventBoardVO vo) throws RemoteException;
+	
+	
+	
 
 }
