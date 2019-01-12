725b9d49568da60583a9a4b24305dc160a682dc4
201809_KHJ <201809_KHJ@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-12 03:10:50 +0000

    뮤직메인 수정
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@54148 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/main/MusicMainController.java
+++ Clap_Client/src/kr/or/ddit/clap/main/MusicMainController.java
@@ -509,11 +509,6 @@ public class MusicMainController implements Initializable {
 	public void salesMange(ActionEvent event) { //매출관리 클릭시
 		   try {
 			   Parent salesManage = FXMLLoader.load(getClass().getResource("../view/ticket/salemanage/salesmanage.fxml")); //바뀔 화면을 가져옴
-			   
-			  /* String temp_path = (getClass().getResource("../view/album/album/ShowAlbumLIst.fxml")).getPath();
-				String path = temp_path.substring(1, temp_path.length()); //현재화면 절대경로
-				gobackStack.goURL(path);  */	
-			   
 			   contents.getChildren().removeAll();
 			   contents.getChildren().setAll(salesManage);
 			   
@@ -521,6 +516,17 @@ public class MusicMainController implements Initializable {
 			   e.printStackTrace();
 		   }
 		   
+	}
+	
+	public void  memManage(){
+		try {
+			   Parent salesManage = FXMLLoader.load(getClass().getResource("../view/member/manage/memmanage.fxml")); //바뀔 화면을 가져옴	
+			   contents.getChildren().removeAll();
+			   contents.getChildren().setAll(salesManage);
+			   
+		   } catch (IOException e) {
+			   e.printStackTrace();
+		   }
 	}
 	
 
---
+++ Clap_Client/src/kr/or/ddit/clap/view/member/manage/MemberManageController.java
@@ -0,0 +1,16 @@
+package kr.or.ddit.clap.view.member.manage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class MemberManageController implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
