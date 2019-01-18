97688d475b73f644dc65f6e1609dbcbbb34ed208
201809_PKH <201809_PKH@d808369f-3623-194b-b0f8-88541be8a525>
2019-01-18 01:35:55 +0000

    memberVO에 게임 항목추가
    git-svn-id: http://112.220.114.130:10001/svn/projects/201809M/team2/source@55651 d808369f-3623-194b-b0f8-88541be8a525

--- Clap_Client/src/kr/or/ddit/clap/vo/member/MemberVO.java
+++ Clap_Client/src/kr/or/ddit/clap/vo/member/MemberVO.java
@@ -24,6 +24,9 @@ public class MemberVO extends RecursiveTreeObject<MemberVO> implements Serializa
    private String mem_black_cnt;
    private String mem_image;
    private String mem_intro;
+   private String mem_game;
+   
+   
    private ImageView imgView;
    
         
@@ -131,5 +134,11 @@ public class MemberVO extends RecursiveTreeObject<MemberVO> implements Serializa
    public void setMem_intro(String mem_intro) {
       this.mem_intro = mem_intro;
    }
+	public String getMem_game() {
+		return mem_game;
+	}
+	public void setMem_game(String mem_game) {
+		this.mem_game = mem_game;
+	}
    
 }
